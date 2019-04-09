a776a108b8eec88e9a2fdfe7351000de693e6e79
twoody <tanner.l.woody@gmail.com>
2019-04-09 14:43:35 -0700

    bugs, varnames, require_once, function name;
    is compiling;
    varname to include `bootstrap` to avoid namespace issues;
    require to require_once;
    cloudflare to popper;

--- js/bootstrap.php
+++ js/bootstrap.php
@@ -1,24 +1,24 @@
 <?
-require('./config/imports.php');
+require_once('./config/imports.php');
 
-function get_scripts(){
+function get_bootstrap_scripts(){
 	$ret = "";
-	if ($HAS_BOOTSTRAP === TRUE){
+	if ($HAS_BOOTSTRAP){
 		$ret .= "\n\t<!-- jQuery first,then Popper.js,then Bootstrap.JS -->"; 
 		$ret .= make_script($BOOTSTRAP_JS_SRC, $BOOTSTRAP_JS_INTEGRITY, $BOOTSTRAP_JS_ORIGIN);
-		$ret .= make_script($BOOTSTRAP_JS_CLOUDFLARE_SRC,$BOOTSTRAP_JS_CLOUDFLARE_INTEGRITY, $BOOTSTRAP_JS_CLOUDFLARE_ORIGIN);
+		$ret .= make_script($BOOTSTRAP_JS_POPPER_SRC,$BOOTSTRAP_JS_POPPER_INTEGRITY, $BOOTSTRAP_JS_POPPER_ORIGIN);
 		$ret .= make_script($BOOTSTRAP_JS_JQUERY_SRC, $BOOTSTRAP_JS_JQUERY_INTEGRITY, $BOOTSTRAP_JS_JQUERY_ORIGIN);
 	}
 	else if($HAS_POPPER || $HAS_JQUERY){
 		if ($HAS_POOPER){
 			$ret .= "\n\t<!-- jQuery first,then Popper.js,then Bootstrap.JS -->"; 
-			$ret .= make_script($BOOTSTRAP_JS_CLOUDFLARE_SRC,$BOOTSTRAP_JS_CLOUDFLARE_INTEGRITY, $BOOTSTRAP_JS_CLOUDFLARE_ORIGIN);
+			$ret .= make_script($BOOTSTRAP_JS_POPPER_SRC,$BOOTSTRAP_JS_POPPER_INTEGRITY, $BOOTSTRAP_JS_POPPER_ORIGIN);
 		}
 		if ($HAS_JQUERY){
 			$ret .= "\n\t<!-- jQuery first,then Popper.js,then Bootstrap.JS -->"; 
 			$ret .= make_script($BOOTSTRAP_JS_JQUERY_SRC, $BOOTSTRAP_JS_JQUERY_INTEGRITY, $BOOTSTRAP_JS_JQUERY_ORIGIN);
 		}
 	}
-	return $ret
+	return $ret;
 }
 ?>
