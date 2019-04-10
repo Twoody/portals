8ec3cafa3451ddeab40d5a3da5b6214dbc6ca0f2
twoody <tanner.l.woody@gmail.com>
2019-04-10 12:26:22 -0700

    imports to use /config/paths.php + get_imports;
    Moved all paths that will be used to /config/paths.php;
    Importing paths and using the var names assigned in paths.php;
    get_imports function init:
    	Function might be a dud and get rm'ed soon;
    	Having difficulty with all of the imports and req'ed Namespaces;

--- config/imports.php
+++ config/imports.php
@@ -1,10 +1,31 @@
 <?php
 	/** ** ** ** ** ** ** GENERAL ** ** ** ** ** **/
-	require_once('./config/settings.php');
-	require_once('./css/bootstrap.php');
-	require_once('./js/bootstrap.php');
+	require_once('./config/paths.php');
+	$PATHS = get_paths();
+
+	require_once($PATHS['SETTINGS_PATH']);
+	require_once($PATHS['BOOTSTRAP_CSS_PATH']);
+	require_once($PATHS['BOOTSTRAP_JS_PATH']);
 
 	/** ** ** ** ** ** ** UTILS ** ** ** ** ** ** **/
-	require_once('./utils/html.php');
-	require_once('./utils/dates.php');
+	require_once($PATHS['HTML_LIB_PATH']);
+	require_once($PATHS['DATES_LIB_PATH']);
+
+
+if (!function_exists("get_imports")){
+	/* FUNCTION WRAPPED IN IF-STMNT BECAUSE MULTIPLE IMPORTS WILL LEAD TO ERRORS; */
+	//TODO: Determine if this function will be necessary or not...
+	function get_imports(){
+		return Array(
+			'./config/paths.php',
+			$PATHS['SETTINGS_PATH'],
+			$PATHS['BOOTSTRAP_CSS_PATH'],
+			$PATHS['BOOTSTRAP_JS_PATH'],
+	
+			/** ** ** ** ** ** ** UTILS ** ** ** ** ** ** **/
+			$PATHS['HTML_LIB_PATH'],
+			$PATHS['DATES_LIB_PATH'],
+		);
+	}
+}
 ?>
