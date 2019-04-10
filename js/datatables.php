690ad373ef15183c0508172bf91c78e0976f8ceb
twoody <tanner.l.woody@gmail.com>
2019-04-10 13:46:12 -0700

    Using CONFIG + testing + working module;
    Only being called upon local changes to CONFIG;
    Commented out testing and usage example;
    Using PATHS;
    HTML comment informing users if imported;

--- js/datatables.php
+++ js/datatables.php
@@ -1,17 +1,26 @@
 <?
 require_once('./config/imports.php');
 
-function get_datatables_scripts(){
+echo "\n<!-- js/datatables.php imported -->\n";
+function get_datatables_scripts($CONFIG=Null){
+	if($CONFIG === Null)
+		$CONFIG	= get_config();
 	$ret = "";
-	if ($HAS_DATATABLES){
-		$ret .= "\n\t<!-- jQuery first,then Popper.js,then Bootstrap.JS -->"; 
-		$ret .= make_script($DATATABLES_JS_SRC, $DATATABLES_JS_INTEGRITY, $DATATABLES_JS_ORIGIN);
-		$ret .= make_script($DATATABLES_JS_JQUERY_SRC, $DATATABLES_JS_JQUERY_INTEGRITY, $DATATABLES_JS_JQUERY_ORIGIN);
+	if ($CONFIG['HAS_DATATABLES']){
+		$ret .= "\n\t<!-- jQuery first,then DataTables.js -->"; 
+		$ret .= make_script($CONFIG['DATATABLES_JS_JQUERY_SRC'],	$CONFIG['DATATABLES_JS_JQUERY_INTEGRITY'],	$CONFIG['DATATABLES_JS_JQUERY_ORIGIN']);
+		$ret .= make_script($CONFIG['DATATABLES_JS_SRC'], 			$CONFIG['DATATABLES_JS_INTEGRITY'],			$CONFIG['DATATABLES_JS_ORIGIN']);
 	}
-	else if($HAS_DATATABLES_JQUERY){
-		$ret .= "\n\t<!-- jQuery first,then Popper.js,then Bootstrap.JS -->"; 
-		$ret .= make_script($DATATABLES_JS_JQUERY_SRC, $DATATABLES_JS_JQUERY_INTEGRITY, $DATATABLES_JS_JQUERY_ORIGIN);
+	else if($CONFIG['HAS_DATATABLES_JQUERY']){
+		$ret .= "\n\t<!-- jQuery first -->"; 
+		$ret .= make_script($CONFIG['DATATABLES_JS_JQUERY_SRC'],	$CONFIG['DATATABLES_JS_JQUERY_INTEGRITY'],	$CONFIG['DATATABLES_JS_JQUERY_ORIGIN']);
 	}
+		$ret .= " \n\t<!-- NO DATATABLES MODULES IMPORTED-->\n";
 	return $ret;
 }
+
+/***** Just for testing *****/
+//$CONFIG	= get_config();
+//$CONFIG['HAS_DATATABLES'] = TRUE;
+//echo get_datatables_scripts($CONFIG) . "\n";
 ?>
