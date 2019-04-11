e5d786ca89de12765c490f88e80483007a972730
twoody <tanner.l.woody@gmail.com>
2019-04-10 23:14:15 -0700

    Proper order of imports for bug fix + var_dump;
    Console was throwing errors because we were out of order;
    Plus an unexpected var_dump o.0;

--- js/bootstrap.php
+++ js/bootstrap.php
@@ -6,12 +6,11 @@ function get_bootstrap_scripts($CONFIG=Null){
 	if($CONFIG === Null)
 		$CONFIG	= get_config();
 	$ret 		= "";
-	var_dump($CONFIG['HAS_BOOTSTRAP']);
 	if ($CONFIG['HAS_BOOTSTRAP']){
 		$ret .= "\n\t<!-- jQuery first,then Popper.js,then Bootstrap.JS -->"; 
-		$ret .= make_script($CONFIG['BOOTSTRAP_JS_SRC'],			$CONFIG['BOOTSTRAP_JS_INTEGRITY'],			$CONFIG['BOOTSTRAP_JS_ORIGIN']);
-		$ret .= make_script($CONFIG['BOOTSTRAP_JS_POPPER_SRC'],	$CONFIG['BOOTSTRAP_JS_POPPER_INTEGRITY'],	$CONFIG['BOOTSTRAP_JS_POPPER_ORIGIN']);
 		$ret .= make_script($CONFIG['BOOTSTRAP_JS_JQUERY_SRC'],	$CONFIG['BOOTSTRAP_JS_JQUERY_INTEGRITY'],	$CONFIG['BOOTSTRAP_JS_JQUERY_ORIGIN']);
+		$ret .= make_script($CONFIG['BOOTSTRAP_JS_POPPER_SRC'],	$CONFIG['BOOTSTRAP_JS_POPPER_INTEGRITY'],	$CONFIG['BOOTSTRAP_JS_POPPER_ORIGIN']);
+		$ret .= make_script($CONFIG['BOOTSTRAP_JS_SRC'],			$CONFIG['BOOTSTRAP_JS_INTEGRITY'],			$CONFIG['BOOTSTRAP_JS_ORIGIN']);
 	}
 	else if($COINFIG['HAS_POPPER'] || $CONFIG['HAS_JQUERY']){
 		if ($CONFIG['HAS_JQUERY']){
