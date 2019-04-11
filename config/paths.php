8cbe8acd0e20623d2d28ea6f16cac67d0d9b4799
twoody <tanner.l.woody@gmail.com>
2019-04-10 23:13:45 -0700

    login template and db_helper;

--- config/paths.php
+++ config/paths.php
@@ -16,6 +16,7 @@ Purpose:
 
 function get_paths(){
 	$ROOT = "/Users/tannerleewoody/Workspace/testdir/php/bootstrap";
+	$ROOT = ".";
 	return Array(
 		'BOOTSTRAP_CSS_PATH'	=> $ROOT.'/css/bootstrap.php',
 		'BOOTSTRAP_JS_PATH'	=> $ROOT.'/js/bootstrap.php',
@@ -25,11 +26,13 @@ function get_paths(){
 		'FORMS_LOGIN'			=> $ROOT."/resources/forms/login.php",
 		'LIBPATH_AUTH_USER'	=> $ROOT.'/utils/auth.php',
 		'LIBPATH_DATES'		=> $ROOT.'/utils/dates.php',
+		'LIBPATH_DB_HELPER'	=> $ROOT.'/utils/db_helper.php',
 		'LIBPATH_HTML'			=> $ROOT.'/utils/html.php',
 		'LIBPATH_JSON'			=> $ROOT.'/utils/json_helper.php',
 		'ROOT'					=> $ROOT,
 		'SETTINGS_PATH'		=> $ROOT.'/config/settings.php',
 		'STRINGS_ERRORS'     => $ROOT."/resources/strings/errors.json",
+		'TEMPLATES_LOGIN'		=> $ROOT."/templates/login.php",
 	);
 }
 ?>
