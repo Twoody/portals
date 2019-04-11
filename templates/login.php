6605e39c16b806c88d9b87e9015c5a4a3d1aa0f3
twoody <tanner.l.woody@gmail.com>
2019-04-11 13:07:09 -0700

    Login page to portray less and use more imports;
    Navbar included;
    moved main function to src/
    Everything compiling;

--- templates/login.php
+++ templates/login.php
@@ -1,88 +1,43 @@
 <!doctype html>
 <?
+/******************************************************************************
+   Author:  Tanner.L.Woody@gmail.com
+   WebLink: 
+   Date:    2019-04-10
+
+USAGE:
+	First Check for compilation errors:
+		php templates/login.php
+	Second, host:
+		clear & php -S localhost:8000 templates/login.php
+Purpose:
+    Display the general layout expected of a login page;
+	 Display the locations where possible params and configs can take place;
+
+******************************************************************************/
+
 require_once('./config/imports.php');
 $PATHS = get_paths();
 
 require_once($PATHS['DATATABLES_CSS_PATH']);
 require_once($PATHS['DATATABLES_JS_PATH']);
-require_once($PATHS['FORMS_LOGIN']);
-require_once($PATHS['LIBPATH_AUTH_USER']);
-require_once($PATHS['LIBPATH_DB_HELPER']);
+require_once($PATHS['HTML_LOGIN_OR_SIGNOUT']);
 $CONFIG = get_config();
 
 /* ----- ----- GENERAL CHANGES BEFORE SECOND IMPORT ----- ----- */
-$CONFIG['TITLE'] = "TEMPLATE LOGIN";	// TODO: Store title in a pages db;
+$CONFIG['TITLE'] = "TEMPLATE LOGIN";
 $CONFIG['HAS_DATATABLES'] = TRUE;
 
-$dbpath		= $PATHS['DB_USERS'];
-$show_login	= true;
-$html			= '';
+$html = '';
 $html .= get_header($CONFIG);
 $html .= "\n";
 $html .= "\n<body>";
+$html .= get_nav($CONFIG);
+$html .= "\n\t<h1>Login Template</h1>";
 $html .= get_js($CONFIG);
 $html .=	"\n\t<!-- Optional JavaScript -->\n";
 
-if(!is_db($dbpath)){
-	//TODO: Alert site is in maintenance;
-	echo clog('"No DB"');	//TODO: Error messages from .../strings/errors.json
-}
-else if(!has_table("users", $dbpath)){
-	//TODO: Alert site is in maintenance;
-	//			Make tables; Populate Users; Ask for user to refresh;
-	echo clog('"No tables"');	//TODO: Error messages from .../strings/errors.json
-}
-else if(isset($_POST['form_submit'])){
-	//TODO: Authenticate user...
-	$show_login			= FALSE;
-	$email				= $_POST["inputEmail"];
-	$user					= strstr($email, '@', true); //strip everything after `@`
-	$pw					= $_POST["inputPassword"];
-	$access				= $_POST["userLevel"];
-	$is_valid_email	= is_valid_email($email);
-	if ($access === "isMember")
-		$access = "member";
-	else if ($access === "isOwner")
-		$access = "owner";
-	else if ($access === "isAdmin")
-		$access = "admin";
-	else{
-		$access = "error";
-		//TODO: Get error pulled from $ERRORS parsed obj of errors.json file;
-		$html.= clog("ERROR 234234: BAD ACCESS LEVEL");
-		$show_login = TRUE;
-	}
-	if ($access !== 'error' && $is_valid_email){
-		$db			= new SQLite3($dbpath);
-		$salt			= get_salt($email);
-		$user_hash	= get_hash($email);
-		$is_validPW	= password_verify($pw.$salt, $user_hash);
-		$has_access	= is_valid_access($email, $access);
-		$db->close();														// CLOSING DB;
-		if (!$has_access || !$is_valid_pw){
-			if (!$is_valid_pw)
-				echo alert("'Bad Password'");
-			else if (!$has_access)
-				echo alert('"Bad Access Level"');
-			else
-				echo alert('"Bad Login"');
-			$show_login = TRUE;
-		}
-	}
-	else{
-		echo alert("Bad Email");
-		$show_login = TRUE;
-	}
-}
-if ($show_login){
-	$html .= get_login_form($CONFIG);
-}
-else{
-	$html .= "<p>Welcome, ".$user.".</p>";
-	$html .= "<p>You are logged in as a ".$access.".</p>";
-	$html .= clog("\"USER: \" + JSON.stringify(" .$user. ")");
-}
-
+$html .= html_login_or_signout($CONFIG, $PATHS);
 $html .= "\n</body>";
 $html .= "\n</html>\n";
 echo $html;
