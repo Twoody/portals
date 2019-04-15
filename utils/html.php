7df5b06a469388bd951c2a9d2da15bbc9d149b22
twoody <tanner.l.woody@gmail.com>
2019-04-15 10:08:38 -0700

    MetaContent + FontAwesome `stacking` + FA styling;
    META_CONTENT keyword to allow config support on html header;
    make_font_awesome_stack function to init icon stacking:
    	i.e. A turd in a spinning circle-notch;
    	i.e. A facebook logo in a circle or square;
    get_font_awesome_style_guide init:
    	Right now, this is all of the custom css we might need;
    	I would like to inlist each element into a database with
    	its corresponding colors, which we could then build on
    	only when we are using them....

--- utils/html.php
+++ utils/html.php
@@ -40,7 +40,79 @@ function get_css($CONFIG=Null){
 		$s .= get_bootstrap_css($CONFIG);
 	if($CONFIG['HAS_DATATABLES'] || $CONFIG['HAS_DATATABLES_JQUERY'] )
 		$s .= get_datatables_css($CONFIG);
+	if($CONFIG['HAS_FONT_AWESOME'])
+		$s .= get_font_awesome_css($CONFIG);
+	$s .= $CONFIG['CUSTOM_STYLES'];
 	return $s;
+}
+function get_font_awesome_style_guide($CONFIG=Null){
+	if($CONFIG === Null)
+		$CONFIG	= get_config();
+	$html = "";
+	$html .= "\n\t\t<style>";
+	$html .= "\n\t\t.fab {";
+	$html .= "\n\t\t  display: inline-block";
+	$html .= "\n\t\t  width: 30px;";
+	$html .= "\n\t\t  height: 30px;";
+	$html .= "\n\t\t  display: inline-block";
+	$html .= "\n\t\t}";
+	$html .= "\n\t\t.fab:hover {";
+	$html .= "\n\t\t    opacity: 0.7;";
+	$html .= "\n\t\t}";
+	$html .= "\n\t\t.fas {";
+	$html .= "\n\t\t  display: inline-block";
+	$html .= "\n\t\t  width: 30px;";
+	$html .= "\n\t\t  height: 30px;";
+	$html .= "\n\t\t  display: inline-block";
+	$html .= "\n\t\t}";
+	$html .= "\n\t\t.fas:hover {";
+	$html .= "\n\t\t    opacity: 0.7;";
+	$html .= "\n\t\t}";
+
+	$html .= "\n\t\t.fa-facebook {color: white;}";
+	$html .= "\n\t\t.fa-twitter {color: white;}";
+	$html .= "\n\t\t.fa-google { color: white;}";
+	$html .= "\n\t\t.fa-linkedin {color: white;}";
+	$html .= "\n\t\t.fa-youtube {color: white;}";
+	$html .= "\n\t\t.fa-instagram {color: white;}";
+	$html .= "\n\t\t.fa-pinterest {color: white;}";
+	$html .= "\n\t\t.fa-snapchat-ghost {color: white;text-shadow: -1px 0 black, 0 1px black, 1px 0 black, 0 -1px black;}";
+	$html .= "\n\t\t.fa-skype {color: white;}";
+	$html .= "\n\t\t.fa-android {color: white;}";
+	$html .= "\n\t\t.fa-dribbble {color: white;}";
+	$html .= "\n\t\t.fa-vimeo {color: white;}";
+	$html .= "\n\t\t.fa-tumblr {color: white;}";
+	$html .= "\n\t\t.fa-vine {color: white;}";
+	$html .= "\n\t\t.fa-foursquare {color: white;}";
+	$html .= "\n\t\t.fa-stumbleupon {color: white;}";
+	$html .= "\n\t\t.fa-flickr {color: white;}";
+	$html .= "\n\t\t.fa-yahoo {color: white;}";
+	$html .= "\n\t\t.fa-soundcloud {color: white;}";
+	$html .= "\n\t\t.fa-reddit {color: white;}";
+	$html .= "\n\t\t.fa-rss {color: white;}";
+	$html .= "\n\t\t.backdrop-facebook			{color:#3B5998; }";
+	$html .= "\n\t\t.backdrop-twitter			{color:#55ACEE; }";
+	$html .= "\n\t\t.backdrop-linkedin			{color:#007bb5; }";
+	$html .= "\n\t\t.backdrop-google				{color:#dd4b39; }";
+	$html .= "\n\t\t.backdrop-youtube			{color:#bb0000; }";
+	$html .= "\n\t\t.backdrop-instagram			{color:#125688; }";
+	$html .= "\n\t\t.backdrop-pinterest			{color:#cb2027; }";
+	$html .= "\n\t\t.backdrop-snapchat-ghost	{color:#fffc00; }";
+	$html .= "\n\t\t.backdrop-skype				{color:#00aff0; }";
+	$html .= "\n\t\t.backdrop-android			{color:#a4c639; }";
+	$html .= "\n\t\t.backdrop-dribbble			{color:#ea4c89; }";
+	$html .= "\n\t\t.backdrop-vimeo				{color:#45bbff; }";
+	$html .= "\n\t\t.backdrop-tumblr				{color:#2c4762; }";
+	$html .= "\n\t\t.backdrop-vine				{color:#00b489; }";
+	$html .= "\n\t\t.backdrop-fouresquare		{color:#45bbff; }";
+	$html .= "\n\t\t.backdrop-stumbleupon		{color:#eb4924; }";
+	$html .= "\n\t\t.backdrop-flickr				{color:#f40083; }";
+	$html .= "\n\t\t.backdrop-yahoo				{color:#430297; }";
+	$html .= "\n\t\t.backdrop-soundcloud		{color:#ff5500; }";
+	$html .= "\n\t\t.backdrop-reddit				{color:#ff5700; }";
+	$html .= "\n\t\t.backdrop-rss					{color:#ff6600; }";
+	$html .= "\n\t\t</style>";
+	return $html;
 }
 
 function get_footer(){
@@ -49,7 +121,6 @@ function get_footer(){
 	$s .= "\n</html>";
 	return $s;
 }
-
 function get_header($CONFIG=Null){
 	if($CONFIG === Null)
 		$CONFIG	= get_config();
@@ -58,7 +129,7 @@ function get_header($CONFIG=Null){
 	$s .=	"\n<head>";
 	$s .=	"\n\t<!-- Required meta tags -->";
 	$s .=	"\n\t<meta charset=\"".$CONFIG['CHAR_SET']."\">";
-	$s .=	"\n\t<meta name=\"viewport\" content=\"width=device-width, initial-scale=1, shrink-to-fit=no\">";
+	$s .=	"\n\t<meta name=\"viewport\" content=\"".$CONFIG['META_CONTENT']."\">";
 	$s .= get_css($CONFIG); //<link> elements
 	$s .= "";
 	$s .=	"\n\t<title>".$CONFIG['TITLE']."</title>";
@@ -124,6 +195,23 @@ function make_css($REL, $LINK, $INTEGRITY="", $ORIGIN=""){
 	$css .= "\n\t\tcrossorigin=\"".$ORIGIN."\">";
 	$css .= "\n\t</link>";
 	return $css;
+}
+function make_font_awesome_stack($icons, $CONFIG=Null){
+	if($CONFIG === Null)
+		$CONFIG	= get_config();
+
+	$stacksize = $CONFIG['FA_STACK_SIZE'];
+	if($CONFIG['FA_STACK_SIZE'])
+		$stacksize = $CONFIG['FA_STACK_SIZE'];
+	$html = "\n\t\t<span class=\"fa-stack ".$stacksize."\">";
+	$c    = count($icons);
+	foreach ($icons as $icon){
+		$icon = "fa-stack-".$c."x " . $icon;
+		$html .= "\n\t\t\t<i class=\"" . $icon . "\"></i>";
+		$c -=1;
+	}
+	$html .= "\n\t\t</span>";
+	return $html;
 }
 function make_par( $s, $args=null ){
 	//Take string `s` and be sure string is properly encapsulated as HTML paragraph
