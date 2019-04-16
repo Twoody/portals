1754094eb5018c4498bc82f1a0fc2e3612666cab
twoody <tanner.l.woody@gmail.com>
2019-04-16 09:08:23 -0700

    Alphabetical order for css styling;

--- utils/font_awesome.php
+++ utils/font_awesome.php
@@ -1,7 +1,7 @@
 <?php
 /******************************************************************************
    Author:  Tanner.L.Woody@gmail.com
-   WebLink: github.com/twoody/phpTests/utils/html.php
+   WebLink: 
    Date:    20181118
 
 USAGE:
@@ -45,48 +45,61 @@ function get_font_awesome_style_guide($CONFIG=Null){
 	$html .= "\n\t\t    opacity: 0.7;";
 	$html .= "\n\t\t}";
 
-	$html .= "\n\t\t.fa-facebook {color: white;}";
-	$html .= "\n\t\t.fa-twitter {color: white;}";
-	$html .= "\n\t\t.fa-google { color: white;}";
-	$html .= "\n\t\t.fa-linkedin {color: white;}";
-	$html .= "\n\t\t.fa-youtube {color: white;}";
-	$html .= "\n\t\t.fa-instagram {color: white;}";
-	$html .= "\n\t\t.fa-pinterest {color: white;}";
-	$html .= "\n\t\t.fa-snapchat-ghost {color: white;text-shadow: -1px 0 black, 0 1px black, 1px 0 black, 0 -1px black;}";
-	$html .= "\n\t\t.fa-skype {color: white;}";
-	$html .= "\n\t\t.fa-android {color: white;}";
-	$html .= "\n\t\t.fa-dribbble {color: white;}";
-	$html .= "\n\t\t.fa-vimeo {color: white;}";
-	$html .= "\n\t\t.fa-tumblr {color: white;}";
-	$html .= "\n\t\t.fa-vine {color: white;}";
-	$html .= "\n\t\t.fa-foursquare {color: white;}";
-	$html .= "\n\t\t.fa-stumbleupon {color: white;}";
-	$html .= "\n\t\t.fa-flickr {color: white;}";
-	$html .= "\n\t\t.fa-yahoo {color: white;}";
-	$html .= "\n\t\t.fa-soundcloud {color: white;}";
-	$html .= "\n\t\t.fa-reddit {color: white;}";
-	$html .= "\n\t\t.fa-rss {color: white;}";
-	$html .= "\n\t\t.backdrop-facebook			{color:#3B5998; }";
-	$html .= "\n\t\t.backdrop-twitter			{color:#55ACEE; }";
-	$html .= "\n\t\t.backdrop-linkedin			{color:#007bb5; }";
-	$html .= "\n\t\t.backdrop-google				{color:#dd4b39; }";
-	$html .= "\n\t\t.backdrop-youtube			{color:#bb0000; }";
-	$html .= "\n\t\t.backdrop-instagram			{color:#125688; }";
-	$html .= "\n\t\t.backdrop-pinterest			{color:#cb2027; }";
-	$html .= "\n\t\t.backdrop-snapchat-ghost	{color:#fffc00; }";
-	$html .= "\n\t\t.backdrop-skype				{color:#00aff0; }";
+	/**** **** **** FA SOCIAL MEDIA ICONS **** **** ****/
+	$html .= "\n\t\t.fa-android						{color: white;}";
+	$html .= "\n\t\t.fa-dribbble						{color: white;}";
+	$html .= "\n\t\t.fa-facebook						{color: white;}";
+	$html .= "\n\t\t.fa-flickr 						{color: white;}";
+	$html .= "\n\t\t.fa-foursquare 					{color: white;}";
+	$html .= "\n\t\t.fa-github							{color: white;}";
+	$html .= "\n\t\t.fa-google							{color: white;}";
+	$html .= "\n\t\t.fa-instagram						{color: white;}";
+	$html .= "\n\t\t.fa-linkedin						{color: white;}";
+	$html .= "\n\t\t.fa-pinterest						{color: white;}";
+	$html .= "\n\t\t.fa-reddit							{color: white;}";
+	$html .= "\n\t\t.fa-rss								{color: white;}";
+	$html .= "\n\t\t.fa-skype							{color: white;}";
+	$html .= "\n\t\t.fa-snapchat-ghost				{color: white; text-shadow: -1px 0 black, 0 1px black, 1px 0 black, 0 -1px black;}";
+	$html .= "\n\t\t.fa-soundcloud					{color: white;}";
+	$html .= "\n\t\t.fa-stack-overflow				{color: white;}";
+	$html .= "\n\t\t.fa-strava							{color: white;}";
+	$html .= "\n\t\t.fa-stumbleupon					{color: white;}";
+	$html .= "\n\t\t.fa-tumblr							{color: white;}";
+	$html .= "\n\t\t.fa-twitter						{color: white;}";
+	$html .= "\n\t\t.fa-vimeo							{color: white;}";
+	$html .= "\n\t\t.fa-vine							{color: white;}";
+	$html .= "\n\t\t.fa-youtube						{color: white;}";
+	$html .= "\n\t\t.fa-yahoo							{color: white;}";
+
+	/**** **** **** FA SOCIAL MEDIA ICONS **** **** ****/
 	$html .= "\n\t\t.backdrop-android			{color:#a4c639; }";
 	$html .= "\n\t\t.backdrop-dribbble			{color:#ea4c89; }";
-	$html .= "\n\t\t.backdrop-vimeo				{color:#45bbff; }";
-	$html .= "\n\t\t.backdrop-tumblr				{color:#2c4762; }";
-	$html .= "\n\t\t.backdrop-vine				{color:#00b489; }";
+	$html .= "\n\t\t.backdrop-facebook			{color:#3B5998; }";
 	$html .= "\n\t\t.backdrop-fouresquare		{color:#45bbff; }";
-	$html .= "\n\t\t.backdrop-stumbleupon		{color:#eb4924; }";
 	$html .= "\n\t\t.backdrop-flickr				{color:#f40083; }";
-	$html .= "\n\t\t.backdrop-yahoo				{color:#430297; }";
-	$html .= "\n\t\t.backdrop-soundcloud		{color:#ff5500; }";
+	$html .= "\n\t\t.backdrop-github				{color:#f40083; }";
+	$html .= "\n\t\t.backdrop-google				{color:#dd4b39; }";
+	$html .= "\n\t\t.backdrop-instagram			{color:#125688; }";
+	$html .= "\n\t\t.backdrop-linkedin			{color:#007bb5; }";
+	$html .= "\n\t\t.backdrop-pinterest			{color:#cb2027; }";
 	$html .= "\n\t\t.backdrop-reddit				{color:#ff5700; }";
 	$html .= "\n\t\t.backdrop-rss					{color:#ff6600; }";
+	$html .= "\n\t\t.backdrop-skype				{color:#00aff0; }";
+	$html .= "\n\t\t.backdrop-snapchat-ghost	{color:#fffc00; }";
+	$html .= "\n\t\t.backdrop-soundcloud		{color:#ff5500; }";
+	$html .= "\n\t\t.backdrop-stack-overflow	{color:#FF9900; }";
+	$html .= "\n\t\t.backdrop-strava				{color:#fc4c02; }";
+	$html .= "\n\t\t.backdrop-stumbleupon		{color:#eb4924; }";
+	$html .= "\n\t\t.backdrop-tumblr				{color:#2c4762; }";
+	$html .= "\n\t\t.backdrop-twitter			{color:#55ACEE; }";
+	$html .= "\n\t\t.backdrop-vimeo				{color:#45bbff; }";
+	$html .= "\n\t\t.backdrop-vine				{color:#00b489; }";
+	$html .= "\n\t\t.backdrop-yahoo				{color:#430297; }";
+	$html .= "\n\t\t.backdrop-youtube			{color:#bb0000; }";
+
+	/**** **** **** FA FOOTER ICONS **** **** ****/
+	$html .= "\n\t\t.backdrop-footer	{color:#646171; }"; //Smokey
+	$html .= "\n\t\t.fa-footer			{color: tomato;}";
 	$html .= "\n\t\t</style>";
 	return $html;
 }
