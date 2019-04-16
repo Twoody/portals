9d315dbd6719e977f1b035f3720c41436a16546c
twoody <tanner.l.woody@gmail.com>
2019-04-16 14:56:35 -0700

    Added footer and margins;

--- templates/login.php
+++ templates/login.php
@@ -22,11 +22,29 @@ $PATHS = get_paths();
 require_once($PATHS['DATATABLES_CSS_PATH']);
 require_once($PATHS['DATATABLES_JS_PATH']);
 require_once($PATHS['HTML_LOGIN_OR_SIGNOUT']);
+require_once($PATHS['LIBPATH_FA']);
 $CONFIG = get_config();
 
 /* ----- ----- GENERAL CHANGES BEFORE SECOND IMPORT ----- ----- */
 $CONFIG['TITLE'] = "TEMPLATE LOGIN";
 $CONFIG['HAS_DATATABLES'] = TRUE;
+$CONFIG['HAS_FONT_AWESOME'] = TRUE;
+$CONFIG['FA_STACK_SIZE'] = 'fa-2x';
+//$CONFIG['FOOTER_IS_STICKY'] = False;
+
+$CONFIG['CUSTOM_STYLES'] .= get_font_awesome_style_guide();
+$CONFIG['CUSTOM_STYLES'] .= "<style>";
+//TODO: Move to $CONFIG and get_css()
+$CONFIG['CUSTOM_STYLES'] .= "\n.bg-primary { background-color: #000 !important;}";
+$CONFIG['CUSTOM_STYLES'] .= "\n.bg-faded { background-color:#ada316  !important;}";
+$CONFIG['CUSTOM_STYLES'] .= "</style>";
+
+//TODO: Move to $CONFIG and get_js()
+$CONFIG['CUSTOM_SCRIPTS'] .= "\n\t\t<script src=\"https://use.fontawesome.com/18fb4b0697.js\"></script>";
+
+
+echo "\n<!-- RUNNING: " . $PATHS['TEMPLATES_LOGIN'] . "-->\n";
+	
 
 $html = '';
 $html .= get_header($CONFIG);
@@ -38,6 +56,7 @@ $html .= get_js($CONFIG);
 $html .=	"\n\t<!-- Optional JavaScript -->\n";
 
 $html .= html_login_or_signout($CONFIG, $PATHS);
+$html .= get_footer($CONFIG);
 $html .= "\n</body>";
 $html .= "\n</html>\n";
 echo $html;
