87c99737a8fa1cb584f13f48e6d477f00f859744
twoody <tanner.l.woody@gmail.com>
2019-04-23 13:06:42 -0700

    $ROOT param + isolation of imports/echos;
    Params for root access added as serving directory
    	is not necessary the working directory...
    All changes to implement the AWD from now on;
    Echos and function calls isolated inside of get_*;

--- css/bootstrap.php
+++ css/bootstrap.php
@@ -1,10 +1,16 @@
 <?
-require_once('./config/imports.php');
-
-echo "\n<!-- css/bootstrap.php imported -->\n";
-function get_bootstrap_css($CONFIG=Null){
+function get_bootstrap_css($CONFIG=Null, $ROOT=Null){
+	if($ROOT === Null && $CONFIG===Null){
+		$ROOT = '.';
+		require_once($ROOT . '/config/paths.php');
+	}
 	if($CONFIG === Null)
-		$CONFIG	= get_config();
+		$CONFIG	= get_config($ROOT);
+	if($ROOT == Null)
+		$ROOT = $CONFIG['ROOT'];
+	$PATHS = get_paths($ROOT);
+	echo "\n<!-- ".$PATHS['BOOTSTRAP_CSS_PATH']." imported -->\n";
+	
 	$bootstrap_css = "";
 	if ($CONFIG['HAS_BOOTSTRAP']) {
 		$bootstrap_css .= "\n\t<!-- BOOTSTRAP CSS -->";
