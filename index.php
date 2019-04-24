b905db2acd688bff8f43764811fa7c152608b7eb
twoody <tanner.l.woody@gmail.com>
2019-04-24 13:52:27 -0700

    All of the stuff 0.o
    html comments for what div closes what;
    ad_button_link added to style guide;
    ad_button_link added to ad's card logo class;
    Whitespace and newlines+tabs;
    row + container + general padding set to zero;
    Container switched to fixed-width;
    Carousel col set to different increments to allocate advertisement card;
    Spans added to make vertical ads on XS screens;
    w-100 breaks added between "rows";

--- index.php
+++ index.php
@@ -1,5 +1,8 @@
+<?php
+session_start();
+?>
 <!doctype html>
-<?
+<?php
 /******************************************************************************
    Author:  Tanner.L.Woody@gmail.com
    WebLink: 
@@ -31,6 +34,7 @@ $CONFIG = get_config($ROOT);
 $CONFIG['CUSTOM_STYLES'] .= "\n<style>";
 $CONFIG['CUSTOM_STYLES'] .= "\n\t.carousel-inner img { width: 50%; height: 50%; margin:auto;}"; 
 $CONFIG['CUSTOM_STYLES'] .= "\n\t.content-slider{display:flex; justify-content:center;}"; 
+$CONFIG['CUSTOM_STYLES'] .= "\n\t.ad_button_link{display:flex; justify-content:center;}"; 
 $CONFIG['CUSTOM_STYLES'] .= "\n</style>";
 $body   = "";
 	
@@ -66,59 +70,78 @@ $pic3 = Array(
 		);
 $pics = Array($pic1, $pic2, $pic3);
 $body .= "\n\t\t";
-$body .= "\n\t\t<div class=\"container content-slider\">";
-//$body .= "\n\t\t<div class=\"row jstify-content-between\">";
-$body .= "\n\t\t<div class=\"row justify-content-between\">";
-$body .= "\n\t\t\t<div class=\"col-sm-2 m-0\"></div>";
-	$body .= "\n\t\t\t<div class=\"col-sm-8 m-0\">";
+$body .= "\n\t\t<div class=\"container fixed-width content-slider p-0\">";
+$body .= "\n\t\t\t<div class=\"row p-0 justify-content-center\">";
+$body .= "\n\t\t\t\t<div class=\"col-12 col-sm-7 col-md-8 col-lg-9 mt-2 p-0\">";
 
-	$body .= "\n\t\t<div id=\"demo\" class=\"carousel slide\" data-ride=\"carousel\">";
-	$body .= "\n\t\t\t<!-- Indicators -->";
-	$body .= "\n\t\t\t<ul class=\"carousel-indicators\">";
-	$body .= "\n\t\t\t\t<li data-target=\"#demo\" data-slide-to=\"0\" class=\"active\"></li>";
-	$body .= "\n\t\t\t\t<li data-target=\"#demo\" data-slide-to=\"1\"></li>";
-	$body .= "\n\t\t\t\t<li data-target=\"#demo\" data-slide-to=\"2\"></li>";
-	$body .= "\n\t\t\t</ul>";
-	$body .= "\n\t\t\t<!-- The slideshow -->";
-	$body .= "\n\t\t\t<div class=\"carousel-inner\">";
-	$body .= get_carousel_items($pics);
+$body .= "\n\t\t\t\t\t<div id=\"demo\" class=\"carousel slide carousel-fade\" data-ride=\"carousel\">";
+$body .= "\n\t\t\t\t\t\t<!-- Indicators -->";
+$body .= "\n\t\t\t\t\t\t<ul class=\"carousel-indicators\">";
+$body .= "\n\t\t\t\t\t\t\t<li data-target=\"#demo\" data-slide-to=\"0\" class=\"active\"></li>";
+$body .= "\n\t\t\t\t\t\t\t<li data-target=\"#demo\" data-slide-to=\"1\"></li>";
+$body .= "\n\t\t\t\t\t\t\t<li data-target=\"#demo\" data-slide-to=\"2\"></li>";
+$body .= "\n\t\t\t\t\t\t</ul>";
+$body .= "\n\t\t\t\t\t\t<!-- The slideshow -->";
+$body .= "\n\t\t\t\t\t\t<div class=\"carousel-inner\">";
+$body .= get_carousel_items($pics);
+$body .= "\n\t\t\t\t\t\t</div>";
 
-	$body .= "\n\t\t</div>";
+$body .= "\n\t\t\t\t\t\t<!-- Left and right controls -->";
+$body .= "\n\t\t\t\t\t\t<a class=\"carousel-control-prev\" href=\"#demo\" role=\"button\" data-slide=\"prev\">";
+$body .= "\n\t\t\t\t\t\t\t<span class=\"carousel-control-prev-icon\"></span>";
+$body .= "\n\t\t\t\t\t\t\t<span class=\"sr-only\">Previous</span>";
+$body .= "\n\t\t\t\t\t\t</a>";
+$body .= "\n\t\t\t\t\t\t<a class=\"carousel-control-next\" href=\"#demo\" role=\"button\" data-slide=\"next\">";
+$body .= "\n\t\t\t\t\t\t\t<span class=\"carousel-control-next-icon\"></span>";
+$body .= "\n\t\t\t\t\t\t\t<span class=\"sr-only\">Next</span>";
+$body .= "\n\t\t\t\t\t\t</a>";
 
-	$body .= "\n\t\t\t<!-- Left and right controls -->";
-	$body .= "\n\t\t\t<a class=\"carousel-control-prev\" href=\"#demo\" role=\"button\" data-slide=\"prev\">";
-	$body .= "\n\t\t\t\t<span class=\"carousel-control-prev-icon\"></span>";
-	$body .= "\n\t\t\t\t<span class=\"sr-only\">Previous</span>";
-	$body .= "\n\t\t\t</a>";
-	$body .= "\n\t\t\t<a class=\"carousel-control-next\" href=\"#demo\" role=\"button\" data-slide=\"next\">";
-	$body .= "\n\t\t\t\t<span class=\"carousel-control-next-icon\"></span>";
-	$body .= "\n\t\t\t\t<span class=\"sr-only\">Next</span>";
-	$body .= "\n\t\t\t</a>";
-	$body .= "\n\t\t</div>";
+$body .= "\n\t\t\t\t\t</div><!-- /.carousel -->";
+$body .= "\n\t\t\t\t</div><!-- /.col -->";
 
-$body .= "\n\t\t</div>";
-//Maybe hide this when so small and add a horizontal ad instead?
-	$body .= "\n\t\t\t<div class=\"col-sm-2 m-0\" style=\"\">";
-	$body .= "\n\t\t\t\t<div class=\"card\" style=\"width:10rem;\">";
-	$body .= "\n\t\t\t\t\t<img class=\"card-img-top\" src=\"./resources/images/Your-Logo-Here-Black-22.jpg\" alt=\"Card image cap\">";
-	$body .= "\n\t\t\t\t\t<div class=\"card-body\">";
-	$body .= "\n\t\t\t\t\t\t<h5 class=\"card-title\">Card title</h5>";
-	$body .= "\n\t\t\t\t\t\t<p class=\"card-text\">";
-	$body .= "\n\t\t\t\t\t\tSome quick example text to build on the card title and make up ";
-	$body .= "the bulk of the card's content.";
-	$body .= "\n\t\t\t\t\t</p>";
-	$body .= "\n\t\t\t\t\t\t<a href=\"#\" class=\"btn btn-primary\">Go somewhere</a>";
-	$body .= "\n\t\t\t\t\t</div>";
-	$body .= "\n\t\t\t\t</div>";
-	$body .= "\n\t\t\t</div>";
+$body .= "\n\t\t\t<span class=\"d-none d-sm-block\">";
+$body .= "\n\t\t\t\t<div class=\"col-sm-3 col-md-2 col-lg-1 mt-2 mr-2\" style=\"\">";
+$body .= "\n\t\t\t\t\t<div class=\"card\" style=\"width:10rem;\">";
+$body .= "\n\t\t\t\t\t\t<img class=\"card-img-top\" src=\"./resources/images/Your-Logo-Here-Black-22.jpg\" alt=\"Card image cap\">";
+$body .= "\n\t\t\t\t\t\t<div class=\"card-body\">";
+$body .= "\n\t\t\t\t\t\t\t<h5 class=\"card-title\">Card title</h5>";
+$body .= "\n\t\t\t\t\t\t\t<p class=\"card-text\">";
+$body .= "\n\t\t\t\t\t\t\tSome quick example text to build on the card title and make up ";
+$body .= "the bulk\t of the card's content.";
+$body .= "\n\t\t\t\t\t\t</p>";
+$body .= "\n\t\t\t\t\t\t\t<a href=\"#\" class=\"btn btn-primary ad_button_link\">$$$</a>";
+$body .= "\n\t\t\t\t\t\t</div>";
+$body .= "\n\t\t\t\t\t</div>";
+$body .= "\n\t\t\t\t</div>";
+$body.= "\n\t\t\t\t</span>";
 
-$body .= "\n\t\t</div>";//end carousel row
+$body .= "\n\t\t\t\t<div class=\"w-100 d-sm-none\"></div>";
 
+$body .= "\n\t\t\t\t<span class=\"d-sm-none\">";
+$body .= "\n\t\t\t\t\t<div class=\"col-12 mx-auto\">";
+$body .= "\n\t\t\t\t\t\t<img src=\"./resources/images/Your-Logo-Here-Black-22.jpg\" class=\"rounded float-right\" width=\"100px\">";
+$body .= "\n\t\t\t\t\t\t</img>";
+$body .= "\n\t\t\t\t\t\t<img src=\"./resources/images/Your-Logo-Here-Black-22.jpg\" class=\"rounded float-left\" width=\"100px\">";
+$body .= "\n\t\t\t\t\t\t</img>";
+$body .= "\n\t\t\t\t\t</div>";
+$body .= "\n\t\t\t\t</span>";
 
-$body .= "\n\t\t</div>"; //End container
+$body .= "\n\t\t\t\t<div class=\"w-100\"></div>";
 
+$body .= "\n\t\t\t\t<div class=\"col-3 d-sm-none bg-success\">";
+$body .= "\n\t\t\t\t\t<div class=\"float-left text-center\">";
+$body .= "\n\t\t\t\t\t\tButton 1 to Link 1";
+$body .= "\n\t\t\t\t\t</div>";
+$body .= "\n\t\t\t\t</div>";
+$body .= "\n\t\t\t\t<div class=\"col-3 d-sm-none bg-warning\">";
+$body .= "\n\t\t\t\t\t<div class=\"float-right text-center\">";
+$body .= "\n\t\t\t\t\t\tButton 2 to Link 2";
+$body .= "\n\t\t\t\t\t</div>";
+$body .= "\n\t\t\t\t</div>";
+
+$body .= "\n\t\t\t</div><!-- /.row -->";
+$body .= "\n\t\t</div><!-- /.container -->";
 echo template_b($body, $CONFIG) . "\n";
-
 
 function get_carousel_items($items){
 	$html = "";
