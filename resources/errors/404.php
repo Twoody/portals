29db36f3a0102704adc02c995c7529388f336fff
twoody <tanner.l.woody@gmail.com>
2019-04-23 21:13:26 -0700

    404.php init + properly working;
    Root and relative paths somewhat figured out;
    I am still having an issue with getting the paths
    	via __init__ calls in utils/ files;

--- resources/errors/404.php
+++ resources/errors/404.php
@@ -1,1 +1,43 @@
-<h1> page was not found, homes.</h1>
+
+<!doctype html>
+<?
+/******************************************************************************
+   Author:  Tanner.L.Woody@gmail.com
+   WebLink: 
+   Date:    2019-04-22
+
+USAGE:
+	First Check for compilation errors:
+		php index.php
+	Second, host:
+		With PHP:
+			clear & php -S localhost:8000 templates/login.php
+		With APACHE:
+			sudo apachectl start
+
+Purpose:
+    Fun way to show that page was not found;
+
+Links:
+	https://www.w3schools.com/bootstrap4/bootstrap_carousel.asp
+******************************************************************************/
+
+$ROOT = "../..";
+require_once($ROOT.'/config/imports.php');
+make_imports($ROOT);
+$PATHS  = get_paths('../../');
+require_once($PATHS['TEMPLATES_B']);
+
+$CONFIG = get_config($ROOT);
+$body   = "";
+	
+/* ----- ----- GENERAL CHANGES BEFORE SECOND IMPORT ----- ----- */
+$CONFIG['TITLE'] = "PG-14 Not Found";
+$CONFIG['DISPLAY_HEADER'] = FALSE;
+
+$body .= "\n\t\t";
+$body .= "<h1> page was not found, homes.</h1>";
+//echo '<!-- '.$PATHS['NAV_HOME'].' -->';
+echo template_b($body, $CONFIG, $PATHS) . "\n";
+
+?>
