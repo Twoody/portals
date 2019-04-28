7ef691dc6d9c6562d4b5e6e2827b82e0903cc43b
twoody <tanner.l.woody@gmail.com>
2019-04-28 08:43:27 -0700

    User Registration form;

---
+++ resources/forms/register.php
@@ -0,0 +1,69 @@
+<?php
+function get_registration_form($CONFIG=Null){
+	if ($CONFIG === Null){
+		$ROOT = ".";
+		require_once($ROOT . '/config/paths.php');
+		$PATHS	= get_paths($ROOT);
+		require_once($PATHS['SETTINGS_PATH']);
+		require_once($PATHS['LIBPATH_HTML']);
+		$CONFIG	= get_config();
+	}
+	$ROOT = $CONFIG['ROOT'];
+	if($PATHS === Null)
+		$PATHS	= get_paths($ROOT);
+	echo "\n<!-- USING FORM: ".$PATHS['FORMS_USER_REGISTRATION']." -->\n";
+
+	$html = "";
+	$html .= "\n\t\t<!------ Include the above in your HEAD tag ---------->";
+	$html .= "\n\t\t<form class=\"form-horizontal\" action=\"".$CONFIG['ACTION_REGISTER']."\" method=\"POST\">";
+	$html .= "\n\t\t\t<fieldset>";
+	$html .= "\n\t\t\t\t<div id=\"legend\">";
+	$html .= "\n\t\t\t\t\t<legend class=\"\">Register</legend>";
+	$html .= "\n\t\t\t\t</div>";
+	$html .= "\n\t\t\t\t<div class=\"control-group\">";
+	$html .= "\n\t\t\t\t\t<!-- Username -->";
+	$html .= "\n\t\t\t\t\t<label class=\"control-label\"\tfor=\"username\">Username</label>";
+	$html .= "\n\t\t\t\t\t<div class=\"controls\">";
+	$html .= "\n\t\t\t\t\t\t<input type=\"text\" id=\"username\" name=\"username\" placeholder=\"\" class=\"input-xlarge\">";
+	$html .= "\n\t\t\t\t\t\t<p class=\"help-block\">Username can contain any letters or numbers, without spaces</p>";
+	$html .= "\n\t\t\t\t\t</div>";
+	$html .= "\n\t\t\t\t</div>";
+	$html .= "\n\t\t\t\t<div class=\"control-group\">";
+	$html .= "\n\t\t\t\t\t<!-- E-mail -->";
+	$html .= "\n\t\t\t\t\t<label class=\"control-label\" for=\"email\">E-mail</label>";
+	$html .= "\n\t\t\t\t\t<div class=\"controls\">";
+	$html .= "\n\t\t\t\t\t\t<input type=\"text\" id=\"email\" name=\"email\" placeholder=\"\" class=\"input-xlarge\">";
+	$html .= "\n\t\t\t\t\t\t<p class=\"help-block\">Please provide your E-mail</p>";
+	$html .= "\n\t\t\t\t\t</div>";
+	$html .= "\n\t\t\t\t</div>";
+	$html .= "\n\t\t ";
+	$html .= "\n\t\t\t\t<div class=\"control-group\">";
+	$html .= "\n\t\t\t\t\t<!-- Password-->";
+	$html .= "\n\t\t\t\t\t<label class=\"control-label\" for=\"password\">Password</label>";
+	$html .= "\n\t\t\t\t\t<div class=\"controls\">";
+	$html .= "\n\t\t\t\t\t\t<input type=\"password\" id=\"password\" name=\"password\" placeholder=\"\" class=\"input-xlarge\">";
+	$html .= "\n\t\t\t\t\t\t<p class=\"help-block\">Password should be at least 4 characters</p>";
+	$html .= "\n\t\t\t\t\t</div>";
+	$html .= "\n\t\t\t\t</div>";
+	$html .= "\n\t\t\t\t<div class=\"control-group\">";
+	$html .= "\n\t\t\t\t\t<!-- Password -->";
+	$html .= "\n\t\t\t\t\t<label class=\"control-label\"\tfor=\"password_confirm\">Password (Confirm)</label>";
+	$html .= "\n\t\t\t\t\t<div class=\"controls\">";
+	$html .= "\n\t\t\t\t\t\t<input type=\"password\" id=\"password_confirm\" name=\"password_confirm\" placeholder=\"\" class=\"input-xlarge\">";
+	$html .= "\n\t\t\t\t\t\t<p class=\"help-block\">Please confirm password</p>";
+	$html .= "\n\t\t\t\t\t</div>";
+	$html .= "\n\t\t\t\t</div>";
+	$html .= "\n\t\t\t\t<div class=\"control-group\">";
+	$html .= "\n\t\t\t\t\t<!-- Button -->";
+	$html .= "\n\t\t\t\t\t<div class=\"controls\">";
+	$html .= "\n\t\t\t\t\t\t<button class=\"btn btn-success\">Register</button>";
+	$html .= "\n\t\t\t\t\t</div>";
+	$html .= "\n\t\t\t\t</div>";
+	$html .= "\n\t\t\t</fieldset>";
+	$html .= "\n\t\t</form>";
+	return $html;
+}
+
+/***** Just for testing *****/
+//echo get_login_form() . "\n";
+?>
