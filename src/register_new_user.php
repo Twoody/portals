e6f44e8cfeb9c26727ad9d8ffca4471f79185534
twoody <tanner.l.woody@gmail.com>
2019-04-28 08:42:01 -0700

    Registration file broken up successfully;
    Form placed in new res/forms/foo.php file;
    Condensed requires into IF/ELSE stmnts based on function calling;
    Started some todo's and flag passing;

--- src/register_new_user.php
+++ src/register_new_user.php
@@ -1,67 +1,45 @@
 <?
-//TODO:
-//	If user is logged in, do not display screen;
-//TODO: Configure to run standalone and in production;
-//$ROOT = '.';
-//require_once('./config/paths.php');
-//$PATHS = get_paths($ROOT);
-//require_once($PATHS['SETTINGS_PATH']);
-//$CONFIG = get_config($ROOT);
-////require_once($PATHS['FORMS_USER_REGISTRATION']);
-//require_once($PATHS['LIBPATH_AUTH_USER']);
-//require_once($PATHS['LIBPATH_DB_HELPER']);
+/******************************************************************************
+   Author:  Tanner.L.Woody@gmail.com
+   WebLink: github.com/twoody/portals/
+   Date:    20190428
 
-echo "\n<!-- ".$PATHS['HTML_REGISTER_USER']." imported -->\n";
+USAGE:
+	Import, make call;
+	Testing env below;
+
+Purpose:
+    Find whether to display registration;
+	 Process user registration request;
+
+******************************************************************************/
 
 function html_register_user($CONFIG=Null, $PATHS=Null){
-	$html = "";
-	$html .= "\n\t\t<!------ Include the above in your HEAD tag ---------->";
-	$html .= "\n\t\t<form class=\"form-horizontal\" action='' method=\"POST\">";
-	$html .= "\n\t\t\t<fieldset>";
-	$html .= "\n\t\t\t\t<div id=\"legend\">";
-	$html .= "\n\t\t\t\t\t<legend class=\"\">Register</legend>";
-	$html .= "\n\t\t\t\t</div>";
-	$html .= "\n\t\t\t\t<div class=\"control-group\">";
-	$html .= "\n\t\t\t\t\t<!-- Username -->";
-	$html .= "\n\t\t\t\t\t<label class=\"control-label\"\tfor=\"username\">Username</label>";
-	$html .= "\n\t\t\t\t\t<div class=\"controls\">";
-	$html .= "\n\t\t\t\t\t\t<input type=\"text\" id=\"username\" name=\"username\" placeholder=\"\" class=\"input-xlarge\">";
-	$html .= "\n\t\t\t\t\t\t<p class=\"help-block\">Username can contain any letters or numbers, without spaces</p>";
-	$html .= "\n\t\t\t\t\t</div>";
-	$html .= "\n\t\t\t\t</div>";
-	$html .= "\n\t\t\t\t<div class=\"control-group\">";
-	$html .= "\n\t\t\t\t\t<!-- E-mail -->";
-	$html .= "\n\t\t\t\t\t<label class=\"control-label\" for=\"email\">E-mail</label>";
-	$html .= "\n\t\t\t\t\t<div class=\"controls\">";
-	$html .= "\n\t\t\t\t\t\t<input type=\"text\" id=\"email\" name=\"email\" placeholder=\"\" class=\"input-xlarge\">";
-	$html .= "\n\t\t\t\t\t\t<p class=\"help-block\">Please provide your E-mail</p>";
-	$html .= "\n\t\t\t\t\t</div>";
-	$html .= "\n\t\t\t\t</div>";
-	$html .= "\n\t\t ";
-	$html .= "\n\t\t\t\t<div class=\"control-group\">";
-	$html .= "\n\t\t\t\t\t<!-- Password-->";
-	$html .= "\n\t\t\t\t\t<label class=\"control-label\" for=\"password\">Password</label>";
-	$html .= "\n\t\t\t\t\t<div class=\"controls\">";
-	$html .= "\n\t\t\t\t\t\t<input type=\"password\" id=\"password\" name=\"password\" placeholder=\"\" class=\"input-xlarge\">";
-	$html .= "\n\t\t\t\t\t\t<p class=\"help-block\">Password should be at least 4 characters</p>";
-	$html .= "\n\t\t\t\t\t</div>";
-	$html .= "\n\t\t\t\t</div>";
-	$html .= "\n\t\t\t\t<div class=\"control-group\">";
-	$html .= "\n\t\t\t\t\t<!-- Password -->";
-	$html .= "\n\t\t\t\t\t<label class=\"control-label\"\tfor=\"password_confirm\">Password (Confirm)</label>";
-	$html .= "\n\t\t\t\t\t<div class=\"controls\">";
-	$html .= "\n\t\t\t\t\t\t<input type=\"password\" id=\"password_confirm\" name=\"password_confirm\" placeholder=\"\" class=\"input-xlarge\">";
-	$html .= "\n\t\t\t\t\t\t<p class=\"help-block\">Please confirm password</p>";
-	$html .= "\n\t\t\t\t\t</div>";
-	$html .= "\n\t\t\t\t</div>";
-	$html .= "\n\t\t\t\t<div class=\"control-group\">";
-	$html .= "\n\t\t\t\t\t<!-- Button -->";
-	$html .= "\n\t\t\t\t\t<div class=\"controls\">";
-	$html .= "\n\t\t\t\t\t\t<button class=\"btn btn-success\">Register</button>";
-	$html .= "\n\t\t\t\t\t</div>";
-	$html .= "\n\t\t\t\t</div>";
-	$html .= "\n\t\t\t</fieldset>";
-	$html .= "\n\t\t</form>";
+	if($CONFIG === Null){
+		$ROOT = ".";
+		require_once($ROOT . '/config/paths.php');
+		$PATHS	= get_paths($ROOT);
+		require_once($PATHS['SETTINGS_PATH']);
+		$CONFIG = get_configt($ROOT);
+		require_once($PATHS['LIBPATH_HTML']);
+	}
+	$ROOT = $CONFIG['ROOT'];
+	$CONFIG['ACTION_REGISTER'] = $PATHS["USER_REGISTER"];
+	if($PATHS === Null)
+		$PATHS	= get_paths($ROOT);
+	echo "\n<!-- ".$PATHS['HTML_REGISTER_USER']." imported -->\n";
+
+	require_once($PATHS['FORMS_USER_REGISTRATION']);
+	require_once($PATHS['LIBPATH_AUTH_USER']);
+	require_once($PATHS['LIBPATH_DB_HELPER']);
+	//TODO: If user is logged in, do not display screen;
+	$display_registration_form = TRUE;
+	$html 	= '';
+	$dbpath	= $PATHS['DB_USERS'];
+
+	if($display_registration_form){
+		$html = get_registration_form($CONFIG);
+	}
 	return  $html;
 }
 
