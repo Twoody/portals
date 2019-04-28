7e374ae384f1982a15eefe7554e4460ed1c9c22c
twoody <tanner.l.woody@gmail.com>
2019-04-27 18:34:20 -0700

    Functions pulled from .../dbs/init.php + params;
    get_users_tables() init;
    has_table() drop paths param;
    make_table init -- pulled from .../dbs/init.php;
    make_users init -- pulled from .../dbs/init.php;

--- utils/db_helper.php
+++ utils/db_helper.php
@@ -28,6 +28,44 @@ $PATHS = get_paths($ROOT);
 require_once($PATHS['LIBPATH_HTML']);
 echo "\n<!-- " . $PATHS['LIBPATH_DB_HELPER'] . " imported -->\n";
 
+function get_users_tables(){
+	$TABLES = Array(
+		'users'=>Array(
+			'id'=>			'INTEGER PRIMARY KEY',
+			'email'=>		'TEXT',
+			'salt'=>			'TEXT',
+			'password'=>	'TEXT',
+			'accessLevel'=>'TEXT'
+		),
+		'userinfo'=> Array(
+			'id'=>						'INTEGER PRIMARY KEY',
+			'fname'=>					'TEXT',
+			'lname'=>					'TEXT',
+			'joindate'=>				'INTEGER',		// Cake day
+			'isActive'=>				'BOOL',			// Did user delete account
+			'lastActive'=>				'INTEGER',		// Date user was last active
+			'isSubscribed'=>			'BOOL',			// Send subscriptions or not
+			'notificationLevel'=>	'INTEGER'		// Best rate of notifications for cont. interaction
+		),
+	);
+	return $TABLES;
+}
+function get_create_table($TNAME, $TABLE){
+	$i      = 0;
+	$fcount = count($TABLE);
+	$sql    = "CREATE TABLE IF NOT EXISTS \"". $TNAME . "\"(";
+	foreach ($TABLE as $FIELD=>$FTYPE){
+		$sql .= " \"" . $FIELD . "\"";
+		$sql .=  " " . $FTYPE;
+		if ($i < $fcount-1)
+			$sql .= ', ';
+		$i +=1;
+	}
+	$sql .= ')';
+	return $sql;
+}
+
+
 function is_db($dbpath, $CONFIG=Null){
 	$ret = TRUE;
 	if($CONFIG === Null)
@@ -48,22 +86,23 @@ function is_db($dbpath, $CONFIG=Null){
 	return $ret;
 }
 
-function has_table($table, $dbpath=Null, $CONFIG=Null, $PATHS=Null){
+function has_table($table, $dbpath=Null, $CONFIG=Null){
 	/*
 	* Verifies whether or not a table exists; 
 	* Defaults to DBPATH_USERS if $dbpath not offered;
 	*/
-	if($CONFIG === Null)
-		$CONFIG = get_config();
-	if($PATHS === Null)
-		$PATHS = get_paths();
-	if ($dbpath === Null)
+	if($CONFIG === Null){
+		$ROOT = '.';
+		$CONFIG = get_config($ROOT);
+	}
+	$PATHS   = get_paths($CONFIG['ROOT']);
+	if($dbpath===Null)
 		$dbpath = $PATHS['DB_USERS'];
 	$FLAGS	= $CONFIG['FLAGS'];
 	$ret		= TRUE;
 	try{
-		$db = new SQLite3($dbpath);
-		$sql = "SELECT name FROM sqlite_master WHERE type='table' AND name=:table";
+		$db	= new SQLite3($dbpath);
+		$sql	= "SELECT name FROM sqlite_master WHERE type='table' AND name=:table";
 		$prepare = $db->prepare($sql);
 		$prepare->bindValue(':table', $table);
 		$result = $prepare->execute();
@@ -78,6 +117,36 @@ function has_table($table, $dbpath=Null, $CONFIG=Null, $PATHS=Null){
 		if (!$FLAGS['is_quite'])
 			echo clog("\"". $exception->getMessage() ."\"");
 		$ret = FALSE;
+	}
+	return $ret;
+}
+function make_table($sql, $dbpath=Null, $CONFIG=Null){
+	if ($CONFIG===Null){
+		$ROOT = '.';
+		$CONFIG = get_config($ROOT);
+	}
+	if($dbpath === Null)
+		$dbpath = $CONFIG['DBPATH_USERS'];
+	$db = new SQLIte3($dbpath);
+	$db->exec($sql);
+	$db->close();
+}
+function make_users_tables($CONFIG){
+	if($CONFIG === Null){
+		$ROOT = '.';
+		$CONFIG = get_config($ROOT);
+	}
+	$ret    = 0;
+	$TABLES = get_users_tables();
+	$PATHS  = get_paths($CONFIG['ROOT']);
+	$dbpath = $CONFIG['DBPATH_USERS'];
+	foreach($TABLES as $TNAME=>$TABLE){
+		if (has_table($TNAME, $dbpath, $CONFIG))
+			continue;
+		echo "\nMAKING TABLE\n";
+		$sql = get_create_table($TNAME, $TABLE);
+		$suc = make_table($sql, $dbpath, $CONFIG);
+		$ret +=1;
 	}
 	return $ret;
 }
