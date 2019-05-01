d1a1575e9ab2d5f82cf4524fda092d7ca2508532
twoody <tanner.l.woody@gmail.com>
2019-04-30 21:58:29 -0700

    home button + chatter + margin to padding;
    Whitespace;
    Margin to paddings;
    columns now in row divs;
    button to a tag with role of buttons;

--- logout/index.php
+++ logout/index.php
@@ -27,7 +27,8 @@ Links:
 $ROOT = "..";
 require_once($ROOT.'/config/imports.php');
 make_imports($ROOT);
-$PATHS  = get_paths($ROOT);
+$PATHS	= get_paths($ROOT);
+$home		= $PATHS['NAV_HOME'];
 require_once($PATHS['TEMPLATES_B']);
 
 $CONFIG = get_config($ROOT);
@@ -41,30 +42,37 @@ $CONFIG['CUSTOM_STYLES'] .= "\n\t.sticky{position: sticky; top: 0;}";
 $CONFIG['CUSTOM_STYLES'] .= "\n</style>";
 
 echo '<!-- RUNNING '.$PATHS['USER_LOGOUT'].' -->';
-$body .= "\n\t\t<div class=\"container fixed-width m-5 sticky\">";
-$body .= "\n\t\t\t<div class=\"row justify-content-center\">";
+
+$body .= "\n\t\t<div class=\"container fixed-width p-5 sticky\">";
 if (is_logged_in($CONFIG)){
 	$_SESSIONS['username'] = Null;
 	$_SESSIONS['loggedin'] = Null;
 	$_SESSIONS['alevel'] = Null;
 	$_SESSIONS['email'] = Null;
 	session_destroy();
+	$body .= "\n\t\t\t<div class=\"row justify-content-center\">";
 	$body .= "\n\t\t\t\t<div class=\"col-12\">";
 	$body .= "\n\t\t\t\t\tSuccessfully logged out";
 	$body .= "\n\t\t\t\t</div>";
+	$body .= "\n\t\t\t</div>";
+	$body .= "\n\t\t\t<div class=\"row justify-content-center\">";
 	$body .= "\n\t\t\t\t<div class=\"col-12\">";
-	$body .= "<button type=\"button\" class=\"btn btn-primary btn-block btn-lg\">";
-	$body .= "\n\t\t\t\t\t<a style=\"color:white;\" href=\"".$PATHS['NAV_HOME']."\">Return home</a>";
-	$body .= "</button>";
+	$body .= "\n\t\t\t\t\t<a role=\"button\" class=\"btn btn-primary btn-block btn-lg\" ";
+	$body .= "href=\"".$home."\"";
+	$body .= ">";
+	$body .= "\n\t\t\t\t\t\tReturn home";
+	$body .= "\n\t\t\t\t\t</a>";
 	$body .= "\n\t\t\t\t</div>";
+	$body .= "\n\t\t\t</div><!-- End row -->";
 }
 else{
 	//user not logged in
+	$body .= "\n\t\t\t<div class=\"row justify-content-center\">";
 	$body .= "\n\t\t\t\t<div class=\"col-12 bg-warning\">";
 	$body .= "\n\t\t\t\t\tWarning, not logged in;";
 	$body .= "\n\t\t\t\t</div>";
+	$body .= "\n\t\t\t</div><!-- End row -->";
 }
-$body .= "\n\t\t\t</div>";
 $body .= "\n\t\t</div>";
 echo template_b($body, $CONFIG, $PATHS) . "\n";
 
