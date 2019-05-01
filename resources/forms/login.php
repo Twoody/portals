7d6fe18d48b8435f47ca5c7cedcc3618f7d106c2
twoody <tanner.l.woody@gmail.com>
2019-04-30 21:51:21 -0700

    Chatter + bug fix + STRINGS + margin to padding;
    Importing STRINGS.php for password mute text;
    Header placed in file above form;
    Changed all margin references to padding references;
    Bug fix on path reference;
    Dropped chatter and commented out msgs;

--- resources/forms/login.php
+++ resources/forms/login.php
@@ -6,33 +6,38 @@ function get_login_form($CONFIG=Null, $PATHS=Null){
 		$PATHS	= get_paths($ROOT);
 		require_once($PATHS['SETTINGS_PATH']);
 		require_once($PATHS['LIBPATH_HTML']);
+		require_once($PATHS['STRINGS_PHP']);
 		$CONFIG	= get_config();
 	}
 	$ROOT = $CONFIG['ROOT'];
 	if($PATHS === Null)
 		$PATHS	= get_paths($ROOT);
+	$STRINGS = get_config_strings($CONFIG);
 	echo "\n<!-- USING FORM: ".$PATHS['FORMS_LOGIN']." -->\n";
 
-	$html  = "\n\t<form action=\"".$CONFIG['ACTION_LOGIN']."\" method=\"post\">";
+	$html = "";
+	$html .= "\n<div class=\"container-fluid p-3\">";
+	$html .= "\n\t<div class=\"row p-3\">";
+	$html .= "\n\t\t<h1>Better dust off that Post-It</h1>";
+	$html .= "\n\t</div>";
+	$html .= "\n\t<form action=\"".$CONFIG['ACTION_LOGIN']."\" method=\"post\">";
 	$html .= "\n\t\t<div class=\"form-group row justify-content-center\">";
-	$html .= "\n\t\t\t<label for=\"inputEmail\"class=\"col-sm-2 col-form-label ml-5\">Email:</label>";
-	$html .= "\n\t\t\t<div class=\"col-sm-8 ml-5\">";
+	$html .= "\n\t\t\t<label for=\"inputEmail\"class=\"col-sm-2 col-form-label pl-5\">Email:</label>";
+	$html .= "\n\t\t\t<div class=\"col-sm-8 pl-5\">";
 	$html .= "\n\t\t\t\t<input type=\"email\" class=\"form-control\" name=\"inputEmail\" id=\"inputEmail\" placeholder=\"Email\">";
 	$html .= "\n\t\t\t</div>";
 	$html .= "\n\t\t</div>";
 	$html .= "\n\t\t<div class=\"form-group row justify-content-center\">";
-	$html .= "\n\t\t\t<label for=\"inputPassword\" class=\"col-sm-2 col-form-label ml-5\">Password:</label>";
-	$html .= "\n\t\t\t<div class=\"col-sm-8 ml-5\">";
+	$html .= "\n\t\t\t<label for=\"inputPassword\" class=\"col-sm-2 col-form-label pl-5\">Password:</label>";
+	$html .= "\n\t\t\t<div class=\"col-sm-8 pl-5\">";
 	$html .= "\n\t\t\t\t<input type=\"password\" class=\"form-control\" name=\"inputPassword\" id=\"inputPassword\" placeholder=\"Password\">";
-	$html .= "\n\t\t\t\t<small id=\"passwordHelpBlock\" class=\"form-text text-muted\">";
-	$html .= "\n\t\t\t\t\tYour password must be 8-20 characters long, contain letters and numbers, and must not contain spaces, special characters, or emoji.";
-	$html .= "\n\t\t\t\t</small>";
+	$html .= "\n\t\t\t\t".$STRINGS['USER_PW_REQS']; //CONTAINS HTML
 	$html .= "\n\t\t\t</div>";
 	$html .= "\n\t\t</div>";
 	$html .= "\n\t\t<fieldset class=\"form-group\">";
 	$html .= "\n\t\t\t<div class=\"row justify-content-center\">";
-	$html .= "\n\t\t\t\t<legend class=\"col-form-label col-sm-2 pt-0 ml-5\">User Level</legend>";
-	$html .= "\n\t\t\t\t<div class=\"col-sm-8 ml-5\">";
+	$html .= "\n\t\t\t\t<legend class=\"col-form-label col-sm-2 pt-0 pl-5\">User Level</legend>";
+	$html .= "\n\t\t\t\t<div class=\"col-sm-8 pl-5\">";
 	$html .= "\n\t\t\t\t\t<div class=\"form-check\">";
 	$html .= "\n\t\t\t\t\t\t<input class=\"form-check-input\" type=\"radio\" name=\"userLevel\" id=\"userLevel\" value=\"isMember\" checked>";
 	$html .= "\n\t\t\t\t\t\t<label class=\"form-check-label\" for=\"userLevel\">";
@@ -58,7 +63,7 @@ function get_login_form($CONFIG=Null, $PATHS=Null){
 
 	$html .= "\n\t\t<div class=\"form-group row justify-content-center\">";
 	$html .= "\n\t\t\t<div class=\"col-sm-2\"></div>";
-	$html .= "\n\t\t\t<div class=\"col-sm-8 ml-5\">";
+	$html .= "\n\t\t\t<div class=\"col-sm-8 pl-5\">";
 	$html .= "\n\t\t\t\t<div class=\"form-check\">";
 	$html .= "\n\t\t\t\t\t<input class=\"form-check-input\" type=\"checkbox\" id=\"isRemembered\">";
 	$html .= "\n\t\t\t\t\t<label class=\"form-check-label\" for=\"gridCheck1\">";
@@ -70,7 +75,7 @@ function get_login_form($CONFIG=Null, $PATHS=Null){
 
 	$html .= "\n\t\t<div class=\"row justify-content-center\">";
 	$html .= "\n\t\t\t<div class=\"form-group row justify-content-center\">";
-	$html .= "\n\t\t\t\t<div class=\"col-sm-6 ml-4 mb-5\">";
+	$html .= "\n\t\t\t\t<div class=\"col-sm-6 pl-4 mb-5\">";
 	$html .= "\n\t\t\t\t\t<button type=\"submit\" class=\"btn btn-primary\" name=\"form_submit\">Sign in</button>";
 	$html .= "\n\t\t\t\t</div>";
 	$html .= "\n\t\t\t</div>";
@@ -78,27 +83,13 @@ function get_login_form($CONFIG=Null, $PATHS=Null){
 
 	$html .= "\n\t\t<div class=\"row justify-content-center\">";
 	$html .= "\n\t\t\t<div class=\"col-sm-2\"></div>";
-	$html .= "\n\t\t\t<div class=\"col-sm-8 ml-5 mb-5\">";
-	$html .= "\n\t\t\t\tNew User? <a href=\"".$PATHS["HTML_REGISTER_USER"]."\">Sign up here.</a>";
+	$html .= "\n\t\t\t<div class=\"col-sm-8 pl-5 mb-5\">";
+	$html .= "\n\t\t\t\tNew User? <a href=\"".$PATHS["USER_REGISTER"]."\">Sign up here.</a>";
 	$html .= "\n\t\t\t</div>";
 	$html .= "\n\t\t</div>";
-
-//	$html .= "\n\t\t<div class=\"row justify-content-center\">";
-//	$html .= "\n\t\t\t<div class=\"col-sm-2\"></div>";
-//	$html .= "\n\t\t\t<div class=\"col-sm-8 ml-5 mb-5\">";
-//	$html .= "\n\t\t\t\tNew User? <a href=\"../foo.php\">Sign up here.</a>";
-//	$html .= "\n\t\t\t</div>";
-//	$html .= "\n\t\t</div>";
-//
-//	$html .= "\n\t\t<div class=\"row justify-content-center\">";
-//	$html .= "\n\t\t\t<div class=\"col-sm-2\"></div>";
-//	$html .= "\n\t\t\t<div class=\"col-sm-8 ml-5 mb-5\">";
-//	$html .= "\n\t\t\t\tNew User? <a href=\"https://facebook.com\">Sign up here.</a>";
-//	$html .= "\n\t\t\t</div>";
-//	$html .= "\n\t\t</div>";
-//
-
 	$html .= "\n\t</form>\n";
+	$html .= "\n</div>";
+
 	return $html;
 }
 
