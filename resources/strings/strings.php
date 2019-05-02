617048c9e3c8396ef385858f8be165436c20538b
twoody <tanner.l.woody@gmail.com>
2019-05-02 11:46:50 -0700

    General messages for user screens;

--- resources/strings/strings.php
+++ resources/strings/strings.php
@@ -9,10 +9,17 @@ function get_config_strings($CONFIG=Null){
 	}
 	$ROOT		= $CONFIG['ROOT'];
 	$PATHS	= get_paths($ROOT);
+	$CONFIG['RESPONSE_CONTAINER'] = "\n<div class=\"container-fluid pr-3 pl-3 m-0\">";
+	$CONFIG['RESPONSE_ROW']			= "\n\t<div class=\"row pl-3 pr-3 m-0\">";
 	echo "\n<!-- RESOURCE FILE: ".$PATHS['STRINGS_PHP']." -->\n";
 
 	return Array(
-		"USER_PW_REQS" => "<small id=\"passwordHelpBlock\" class=\"form-text text-muted\">Your password must be 8-20 characters long, contain letters and numbers, and must not contain spaces, special characters, or emoji.</small>",
+		"USER_INVALID_PERMISSION"=> $CONFIG['RESPONSE_CONTAINER'] .  $CONFIG['RESPONSE_ROW'] .  "\n\t\t\t\t<div class=\"col-12 bg-warning\">\n\t\t\t\t\tWarning: Access Denied;\n\t\t\t\t</div>\n\t\t\t</div><!-- End row -->\n\t\t</div><!-- End container -->",
+
+		"USER_NOT_LOGGED_IN"=> $CONFIG['RESPONSE_CONTAINER'] .  $CONFIG['RESPONSE_ROW'] .  "\n\t\t\t\t<div class=\"col-12 bg-warning\">\n\t\t\t\t\tWarning, not logged in;\n\t\t\t\t</div>\n\t\t\t</div><!-- End row -->\n\t\t</div><!-- End container -->",
+
+		"USER_PW_REQS" => "\n<small id=\"passwordHelpBlock\" class=\"form-text text-muted\">Your password must be 8-20 characters long, contain letters and numbers, and must not contain spaces, special characters, or emoji.</small>\n",
+
 	);
 }
 
