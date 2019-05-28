0c547cf5607e7b673074b7da1e92cbf373d63210
twoody <tanner.l.woody@gmail.com>
2019-05-28 13:26:06 -0700

    Features dropdown organization and split button;
    Drop down is now using array object from dd_texts.php;
    Object has 'title', 'code', and 'funct' to help distiguish and focus on
    	a single point of entry;
    ID being introduced to split button text;
    Split button text being mutated via res/js/offline/dd.js;

--- features/dropdown_texts.php
+++ features/dropdown_texts.php
@@ -1,5 +1,5 @@
 <?php
-/*
+/*******************************************************************************
 Author:
 	Tanner.L.Woody@gmail.com
 	2019-05-27
@@ -12,73 +12,169 @@ Usage:
 
 Purpose:	
 	Key value pair to load content;
-*/
-function get_dropdown_items($CONFIG){
-	//These are the items in specifics to making this website;
-	$dropdown_items_arr	= Array(
-		'Reset',
-		'Cloud Computing',
-		'Cookies',
-		'Database Management',
-		'Demos and Pilots',
-		'Dev Tools',
-		'Image and File Uploading',
-		'Image Manipulation and Processing (Aspect Ratio & Mores)',
-		'ISO Formatting',
-		'jQuery',
-		'Java',
-		'Javascript',
-		'Many-to-Many Relationships',
-		'Minimum Viable Product',
-		'Multi-Lingual Support',
-		'Not Wordpress',
-		'OWASP Security Standards',
-		'PHP',
-		'Regular Expression Mastery',
-		'Post, Redirect, Get + JS Form Nullifier',
-		'Templates',
-		'Test Driven Development',
-		'User Experience (UX)',
-		'User Management',
-		'User Workflow',
-		'Version Control (GIT)',
-		'VIM',
-	);
-	return $dropdown_items_arr;
-}
-function get_dropdown_texts($CONFIG){
-	return Array(
-		'reset'=>make_lorem_ipsum(1),
-		'cloud_computing'=>get_f_text($CONFIG),
-		'cookies'=>get_f_text($CONFIG),
-		'database_management'=>get_f_text($CONFIG),
-		'demos_and_pilots'=>get_f_text($CONFIG),
-		'dev_tools'=>get_f_text($CONFIG),
-		'image_and_file_uploading'=>get_f_text($CONFIG),
-		'image_manipulation_and_processing'=>get_f_text($CONFIG),
-		'iso_formatting'=>get_f_text($CONFIG),
-		'jquery'=>get_f_text($CONFIG),
-		'javascript'=>get_f_text($CONFIG),
-		'many_to_many_relationships'=>get_f_text($CONFIG),
-		'minimum_viable_product'=>get_f_text($CONFIG),
-		'multi_lingual_support'=>get_f_text($CONFIG),
-		'not_wordpress'=>get_f_text($CONFIG),
-		'owasp_security_standards'=>get_f_text($CONFIG),
-		'php'=>get_f_text($CONFIG),
-		'post_redirect_get'=>get_f_text($CONFIG),
-		'templates'=>get_f_text($CONFIG),
-		'test_driven_development'=>get_f_text($CONFIG),
-		'user_experience'=>get_f_text($CONFIG),
-		'user_management'=>get_f_text($CONFIG),
-		'user_workflow'=>get_f_text($CONFIG),
-		'git'=>get_f_text($CONFIG),
-		'vim'=>get_f_text($CONFIG),
-	);
-}
+*******************************************************************************/
 function get_f_text($CONFIG){
 	$ret = 'Not registerd yet...';
 	$ret .= "Pleaes understand that there are 10+ items above that we need to process and make into their own select text and mini-blog post. " ;
 	$ret .= "We will soon have all of the mappings put here with specifics, until then, enjoy the fun \$_GET seen here!";
 	return $ret;
 }
+function get_dropdown_text($key){
+	//Return the text associated with key
+	$items	= get_dropdown_items($CONFIG);
+	$text		= "";
+	for ($i=0; $i<count($items); $i++){
+		$item = $items[$i];
+		if ($item['code'] === $key){
+			$text	= $item['funct'];
+			break;
+		}
+	}//end i-for
+	return $text;
+}
+function is_key_in_items($key){
+	$items	= get_dropdown_items($CONFIG);
+	for ($i=0; $i<count($items); $i++){
+		$item = $items[$i];
+		if ($item['code'] === $key)
+			return True;
+	}//end i-for
+	return False;
+}
+function get_dropdown_items($CONFIG){
+	$foo	= Array(
+		1=>Array(
+				'title'=>'Reset',  			
+				'code'=>'reset',                              			
+	 			'funct'=>make_lorem_ipsum(1, $CONFIG),
+		),
+	 	2=>Array(
+	     		'title'=>'Cloud Computing', 			
+				'code'=>'cloud_computing',
+				'funct'=>get_f_text($CONFIG),
+		),
+	 	3=>Array(
+	     		'title'=>'Cookies', 			
+				'code'=>'cookies',                            		
+	 			'funct'=>get_f_text($CONFIG),
+		),
+	 	4=>Array(
+	     		'title'=>'Database Management',  			
+				'code'=>'database_management',                		
+	 			'funct'=>get_f_text($CONFIG),
+		),
+	 	5=>Array(
+	     		'title'=>'Demos and Pilots',  			
+				'code'=>'demos_and_pilots',                   		
+	 			'funct'=>get_f_text($CONFIG),
+		),
+	 	6=>Array(
+	     		'title'=>'Dev Tools', 			
+				'code'=>'dev_tools',                          		
+	 			'funct'=>get_f_text($CONFIG),
+		),
+	 	7=>Array(
+	     		'title'=>'Image and File Uploading', 			
+				'code'=>'image_and_file_uploading',           		
+	 			'funct'=>get_f_text($CONFIG),
+		),
+	 	8=>Array(
+	     		'title'=>'Image Manipulation and Processing (Aspect Ratio & Mores)',			
+				'code'=>'image_manipulation_and_processing',  		
+	 			'funct'=>get_f_text($CONFIG),
+		),
+	 	9=>Array(
+	     		'title'=>'ISO Formatting',  			
+				'code'=>'iso_formatting',                     		
+	 			'funct'=>get_f_text($CONFIG),
+		),
+	 	10=>Array(
+	     		'title'=>'jQuery', 			
+				'code'=>'jquery',                             		 
+	 			'funct'=>get_f_text($CONFIG),
+		),
+	 	12=>Array(
+	     		'title'=>'Javascript',  			
+				'code'=>'javascript',                         		 
+	 			'funct'=>get_f_text($CONFIG),
+		),
+	 	13=>Array(
+	     		'title'=>'Many-to-Many Relationships', 			
+				'code'=>'many_to_many_relationships',         		 
+	 			'funct'=>get_f_text($CONFIG),
+		),
+	 	14=>Array(
+	     		'title'=>'Minimum Viable Product', 			
+				'code'=>'minimum_viable_product',             		 
+	 			'funct'=>get_f_text($CONFIG),
+		),
+	 	15=>Array(
+	     		'title'=>'Multi-Lingual Support',  			
+				'code'=>'multi_lingual_support',              		 
+	 			'funct'=>get_f_text($CONFIG),
+		),
+	 	16=>Array(
+	     		'title'=>'Not Wordpress', 			
+				'code'=>'not_wordpress',                      		 
+	 			'funct'=>get_f_text($CONFIG),
+		),
+	 	17=>Array(
+	     		'title'=>'OWASP Security Standards', 			
+				'code'=>'owasp_security_standards',           		 
+	 			'funct'=>get_f_text($CONFIG),
+		),
+	 	18=>Array(
+	     		'title'=>'PHP',  			
+				'code'=>'php',                                		 
+	 			'funct'=>get_f_text($CONFIG),
+		),
+	 	19=>Array(
+	     		'title'=>'Regular Expression Mastery', 			
+	     		'title'=>'regex',
+	 			'funct'=>get_f_text($CONFIG),
+		),
+	 	20=>Array(
+	     		'title'=>'Post, Redirect, Gets JS Form Nullifier', 
+				'code'=>'post_redirect_get',                  		 
+	 			'funct'=>get_f_text($CONFIG),
+		),
+	 	21=>Array(
+	     		'title'=>'Templates', 			
+				'code'=>'templates',                          		 
+	 			'funct'=>get_f_text($CONFIG),
+		),
+	 	22=>Array(
+	     		'title'=>'Test Driven Development',  			
+				'code'=>'test_driven_development',            		 
+	 			'funct'=>get_f_text($CONFIG),
+		),
+	 	23=>Array(
+	     		'title'=>'User Experience (UX)', 			
+				'code'=>'user_experience',                    		 
+	 			'funct'=>get_f_text($CONFIG),
+		),
+	 	24=>Array(
+	     		'title'=>'User Management', 			
+				'code'=>'user_management',                    		 
+	 			'funct'=>get_f_text($CONFIG),
+		),
+	 	25=>Array(
+	     		'title'=>'User Workflow', 			
+				'code'=>'user_workflow',                      		 
+	 			'funct'=>get_f_text($CONFIG),
+		),
+	 	26=>Array(
+	     		'title'=>'Version Control (GIT)',  			
+				'code'=>'git',                                		 
+	 			'funct'=>get_f_text($CONFIG),
+		),
+	  	27=>Array(
+				'title'=>'VIM',  			
+				'code'=>'vim',                                		 
+	  			'funct'=>get_f_text($CONFIG),
+		),
+	);
+	return $foo;
+}
+
 ?>
--- features/index.php
+++ features/index.php
@@ -54,30 +54,30 @@ $ad_not_sm_arr	= Array(
 	'class'=>"d-none d-sm-block",
 	'content'=>$ad_not_sm_content,
 );
-$ad_not_sm		= make_tag('span', $ad_not_sm_arr, $CONFIG);
-$dropdown_items_arr	= get_dropdown_items($CONFIG);
-$dropdown_texts	= get_dropdown_texts($CONFIG);
-$goodReq = False;
-if(isset($_GET['display'])){
-	//dropdown.php function call to get key-value of `display`
-	$display_req	= $_GET['display'];
-	if(array_key_exists($display_req, $dropdown_texts)){
-		$main_content	= $dropdown_texts[$display_req];
-		$goodReq = True;
+$ad_not_sm				= make_tag('span', $ad_not_sm_arr, $CONFIG);
+$dropdown_items		= get_dropdown_items($CONFIG);
+$goodReq					= False;
+if(isset($_GET['display_code'])){
+	$display_req	= $_GET['display_code'];
+	if(is_key_in_items($display_req)){
+		$main_content	= get_dropdown_text($display_req);
+		$goodReq			= True;
 	}
 }
 if ($goodReq === False)
 	$main_content	= make_lorem_ipsum(1);
 
-$dropdown_items	= "";
+$dropdown_items		= "";
+$dropdown_items_arr	= get_dropdown_items($CONFIG);
 for ($i=0; $i<count($dropdown_items_arr); $i++){
-	$item	= $dropdown_items_arr[$i];
-	$getReq	= strtolower($item);
-	$getReq	= str_replace(' ', '_', $getReq);
+	echo "\n<!-- MEAT: " . $item['title'] ." -->";
+	$item				= $dropdown_items_arr[$i];
+	$display_code	= "display_code=" . $item['code'];
+	$display_title	= "display_title=" . $item['title'];
 	$dropdown_item_arr	= Array(
 		'class'=>"dropdown-item",
-		'href'=>$PATHS['FEATUERS']."?display=".$getReq ,
-		'content'=>$item,
+		'href'=>$PATHS['FEATUERS']."?" . $display_code . "&" . $display_title,
+		'content'=>$item['title'],
 	);
 	$dropdown_items	.= "\n\t" . make_tag('a', $dropdown_item_arr, $CONFIG);
 }//end i-for
@@ -87,6 +87,7 @@ $dropdown_menu_arr	= Array(
 );
 $dropdown_menu	= make_tag('div', $dropdown_menu_arr, $CONFIG);
 $dd_desc_arr	= Array(
+	'id'=>'TITLE_CHANGER',
 	'type'=>'button',
 	'class'=>'btn btn-danger',
 	'content'=>'Make UR Own Site',
@@ -151,6 +152,12 @@ $container0	= make_tag("div", $container0_arr, $CONFIG);
 $container1	= make_gen_container(get_ads_sm($CONFIG), $CONFIG);
 $body .= $container0;
 $body .= $container1;
+
+$js_arr	= Array(
+	'type'=>'text/javascript',
+	'src'=>$PATHS['FEATURES_JS'],
+);
+$body .= make_tag('script', $js_arr, $CONFIG);
 
 $CONFIG['BODY'] = $body;
 echo template_b($CONFIG) . "\n";
