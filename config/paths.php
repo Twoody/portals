dbe05998a31826da1784c9435fa80a73678ea6ea
twoody <tanner.l.woody@gmail.com>
2019-05-27 22:24:08 -0700

    Features index.php page;

--- config/paths.php
+++ config/paths.php
@@ -28,6 +28,7 @@ function get_paths($ROOT=Null){
 		'DATATABLES_JS_PATH'				=> $ROOT.'/resources/js/datatables.php',
 		'DB_USERS'							=> $ROOT."/resources/dbs/users.db",
 		'DB_INVENTORY'						=> $ROOT."/resources/dbs/inventory.db",
+		'FEATURES'							=> $ROOT."/features/index.php",
 		'FONT_AWESOME_CSS_PATH'			=> $ROOT.'/resources/css/font_awesome.php',
 		'FONT_AWESOME_JS_PATH'			=> $ROOT.'/resources/js/font_awesome.php',
 		'FORMS_ADMIN_VIEWPORT'			=> $ROOT."/resources/forms/admin_viewport.php",
