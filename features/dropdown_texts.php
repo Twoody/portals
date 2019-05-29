c5a9634a1622296ae6568338a66a9595692bd2a9
twoody <tanner.l.woody@gmail.com>
2019-05-29 14:52:34 -0700

    get_file_text init + dropdown items bug fix;
    get_cloud_text() to generic get_file_text($filepath);
    dropdown_items_arr accurately numbered elements in array:
    	This fixes a bug where not all of the elements would show from
    	the array as we were for looping them iterably instead of
    	iterating the keys;

--- features/dropdown_texts.php
+++ features/dropdown_texts.php
@@ -10,13 +10,13 @@ Usage:
 	require($PATHS['FEATURES_DD_TEXT']);
 	$dd_text	= get_dropdown_texts($CONFIG);
 
-Purpose:	
+Purpose:
 	Key value pair to load content;
 *******************************************************************************/
 function get_f_text($CONFIG){
 	$content = 'Not registerd yet...';
-	$content .= "Pleaes understand that there are 10+ items above that we need to process and make into their own select text and mini-blog post. " ;
-	$content .= "We will soon have all of the mappings put here with specifics, until then, enjoy the fun \$_GET seen here!";
+	$content .= "Pleaes understand that there are 20+ items above that we need to process and make into their own select text and mini-blog post. " ;
+	$content .= "We will soon have all of the mappings put here with specifics, until then, enjoy the fun <code>\$_GET</code> seen here!";
 	$p	= make_tag('p', Array('content'=>$content), $CONFIG);
 	return $p;
 }
@@ -26,9 +26,9 @@ function get_reset_text($CONFIG){
 	$p	= make_tag('p', Array('content'=>$content), $CONFIG);
 	return $p;
 }
-function get_cloud_text($CONFIG){
-	$PATHS	= get_paths($CONFIG['ROOT']);
-	$text	= file_get_contents($PATHS['LONG_TEXT_CLOUD_COMPUTING']);
+function get_file_text($filepath, $CONFIG){
+	$text		= file_get_contents($filepath);
+	//$text		= 'foo';
 	return $text;
 }
 function get_dropdown_text($key, $CONFIG){
@@ -48,141 +48,145 @@ function is_key_in_items($key,$CONFIG){
 	$items	= get_dropdown_items($CONFIG);
 	for ($i=0; $i<count($items); $i++){
 		$item = $items[$i];
-		if ($item['code'] === $key)
+		$code	= $item['code'];
+		if ($code === $key){
 			return True;
+		}
 	}//end i-for
+	echo "\n<!-- MEAT233: `".$key."` -->";
 	return False;
 }
 function get_dropdown_items($CONFIG){
+	$PATHS	= get_paths($CONFIG['ROOT']);
 	$foo	= Array(
-		1=>Array(
-				'title'=>'Reset',  			
-				'code'=>'reset',                              			
+		0=>Array(
+				'title'=>'Reset',
+				'code'=>'reset',
 	 			'funct'=>get_reset_text($CONFIG) . make_lorem_ipsum(1, $CONFIG),
 		),
-	 	2=>Array(
-	     		'title'=>'Cloud Computing', 			
+	 	1=>Array(
+	     		'title'=>'Cloud Computing',
 				'code'=>'cloud_computing',
-				'funct'=>get_cloud_text($CONFIG),
+				'funct'=>get_file_text($PATHS['LONG_TEXT_CLOUD_COMPUTING'], $CONFIG),
+		),
+	 	2=>Array(
+	     		'title'=>'Cookies',
+				'code'=>'cookies',
+	 			'funct'=>get_file_text($PATHS['LONG_TEXT_COOKIES'],$CONFIG),
 		),
 	 	3=>Array(
-	     		'title'=>'Cookies', 			
-				'code'=>'cookies',                            		
+	     		'title'=>'Database Management',
+				'code'=>'database_management',
 	 			'funct'=>get_f_text($CONFIG),
 		),
 	 	4=>Array(
-	     		'title'=>'Database Management',  			
-				'code'=>'database_management',                		
+	     		'title'=>'Demos and Pilots',
+				'code'=>'demos_and_pilots',
 	 			'funct'=>get_f_text($CONFIG),
 		),
 	 	5=>Array(
-	     		'title'=>'Demos and Pilots',  			
-				'code'=>'demos_and_pilots',                   		
+	     		'title'=>'Dev Tools',
+				'code'=>'dev_tools',
 	 			'funct'=>get_f_text($CONFIG),
 		),
 	 	6=>Array(
-	     		'title'=>'Dev Tools', 			
-				'code'=>'dev_tools',                          		
+	     		'title'=>'Image and File Uploading',
+				'code'=>'image_and_file_uploading',
 	 			'funct'=>get_f_text($CONFIG),
 		),
 	 	7=>Array(
-	     		'title'=>'Image and File Uploading', 			
-				'code'=>'image_and_file_uploading',           		
+	     		'title'=>'Image Manipulation and Processing (Aspect Ratio & Mores)',
+				'code'=>'image_manipulation_and_processing',
 	 			'funct'=>get_f_text($CONFIG),
 		),
 	 	8=>Array(
-	     		'title'=>'Image Manipulation and Processing (Aspect Ratio & Mores)',			
-				'code'=>'image_manipulation_and_processing',  		
+	     		'title'=>'ISO Formatting',
+				'code'=>'iso_formatting',
 	 			'funct'=>get_f_text($CONFIG),
 		),
-	 	9=>Array(
-	     		'title'=>'ISO Formatting',  			
-				'code'=>'iso_formatting',                     		
+	 	9 =>Array(
+	     		'title'=>'jQuery',
+				'code'=>'jquery',
 	 			'funct'=>get_f_text($CONFIG),
 		),
 	 	10=>Array(
-	     		'title'=>'jQuery', 			
-				'code'=>'jquery',                             		 
+	     		'title'=>'Javascript',
+				'code'=>'javascript',
+	 			'funct'=>get_f_text($CONFIG),
+		),
+	 	11=>Array(
+	     		'title'=>'Many-to-Many Relationships',
+				'code'=>'many_to_many_relationships',
 	 			'funct'=>get_f_text($CONFIG),
 		),
 	 	12=>Array(
-	     		'title'=>'Javascript',  			
-				'code'=>'javascript',                         		 
+	     		'title'=>'Minimum Viable Product',
+				'code'=>'minimum_viable_product',
 	 			'funct'=>get_f_text($CONFIG),
 		),
 	 	13=>Array(
-	     		'title'=>'Many-to-Many Relationships', 			
-				'code'=>'many_to_many_relationships',         		 
+	     		'title'=>'Multi-Lingual Support',
+				'code'=>'multi_lingual_support',
 	 			'funct'=>get_f_text($CONFIG),
 		),
 	 	14=>Array(
-	     		'title'=>'Minimum Viable Product', 			
-				'code'=>'minimum_viable_product',             		 
+	     		'title'=>'Not Wordpress',
+				'code'=>'not_wordpress',
 	 			'funct'=>get_f_text($CONFIG),
 		),
 	 	15=>Array(
-	     		'title'=>'Multi-Lingual Support',  			
-				'code'=>'multi_lingual_support',              		 
+	     		'title'=>'OWASP Security Standards',
+				'code'=>'owasp_security_standards',
 	 			'funct'=>get_f_text($CONFIG),
 		),
 	 	16=>Array(
-	     		'title'=>'Not Wordpress', 			
-				'code'=>'not_wordpress',                      		 
+	     		'title'=>'PHP',
+				'code'=>'php',
 	 			'funct'=>get_f_text($CONFIG),
 		),
 	 	17=>Array(
-	     		'title'=>'OWASP Security Standards', 			
-				'code'=>'owasp_security_standards',           		 
-	 			'funct'=>get_f_text($CONFIG),
-		),
-	 	18=>Array(
-	     		'title'=>'PHP',  			
-				'code'=>'php',                                		 
-	 			'funct'=>get_f_text($CONFIG),
-		),
-	 	19=>Array(
-	     		'title'=>'Regular Expression Mastery', 			
+	     		'title'=>'Regular Expression Mastery',
 	     		'title'=>'regex',
 	 			'funct'=>get_f_text($CONFIG),
 		),
+	 	18=>Array(
+	     		'title'=>'Post, Redirect, Gets JS Form Nullifier',
+				'code'=>'post_redirect_get',
+	 			'funct'=>get_f_text($CONFIG),
+		),
+	 	19=>Array(
+	     		'title'=>'Templates',
+				'code'=>'templates',
+	 			'funct'=>get_f_text($CONFIG),
+		),
 	 	20=>Array(
-	     		'title'=>'Post, Redirect, Gets JS Form Nullifier', 
-				'code'=>'post_redirect_get',                  		 
+	     		'title'=>'Test Driven Development',
+				'code'=>'test_driven_development',
 	 			'funct'=>get_f_text($CONFIG),
 		),
 	 	21=>Array(
-	     		'title'=>'Templates', 			
-				'code'=>'templates',                          		 
+	     		'title'=>'User Experience (UX)',
+				'code'=>'user_experience',
 	 			'funct'=>get_f_text($CONFIG),
 		),
 	 	22=>Array(
-	     		'title'=>'Test Driven Development',  			
-				'code'=>'test_driven_development',            		 
+	     		'title'=>'User Management',
+				'code'=>'user_management',
 	 			'funct'=>get_f_text($CONFIG),
 		),
 	 	23=>Array(
-	     		'title'=>'User Experience (UX)', 			
-				'code'=>'user_experience',                    		 
+	     		'title'=>'User Workflow',
+				'code'=>'user_workflow',
 	 			'funct'=>get_f_text($CONFIG),
 		),
 	 	24=>Array(
-	     		'title'=>'User Management', 			
-				'code'=>'user_management',                    		 
-	 			'funct'=>get_f_text($CONFIG),
+	     		'title'=>'Version Control',
+				'code'=>'git',
+				'funct'=>get_file_text($PATHS['LONG_TEXT_GITHUB'], $CONFIG),
 		),
-	 	25=>Array(
-	     		'title'=>'User Workflow', 			
-				'code'=>'user_workflow',                      		 
-	 			'funct'=>get_f_text($CONFIG),
-		),
-	 	26=>Array(
-	     		'title'=>'Version Control (GIT)',  			
-				'code'=>'git',                                		 
-	 			'funct'=>get_f_text($CONFIG),
-		),
-	  	27=>Array(
-				'title'=>'VIM',  			
-				'code'=>'vim',                                		 
+	  	25=>Array(
+				'title'=>'VIM',
+				'code'=>'vim',
 	  			'funct'=>get_f_text($CONFIG),
 		),
 	);
