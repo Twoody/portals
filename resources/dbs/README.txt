a85144384fd93b5762b8787be43938d86febba73
twoody <tanner.l.woody@gmail.com>
2019-05-30 09:06:45 -0700

    init file for resources.sql & README;
    README to help with some sqlite3 command line usage;
    README to help with .php file nuances;
    resources.sql for blogs, comments, and first blog;

---
+++ resources/dbs/README.txt
@@ -0,0 +1,17 @@
+Author:
+	Tanner.L.Woody@gmail.com
+	2019-05-30
+
+Purpose:
+	Explain some nuances and provide guidelines for specific language
+	calls and expected functionality.
+
+Nuances of init*.php files:
+	Do they work? What do they do? Can they always be freely executed,
+	or do these files lack validation and sanity checks?
+
+Common Commands:
+	Read and Execute a *.sql file:
+		sqlite3 some.db
+		sqlite> .read my_creates.sql;
+	
---
+++ resources/dbs/init_resources.sql
@@ -0,0 +1,61 @@
+CREATE TABLE IF NOT EXISTS blogs(
+	id 			INTEGER PRIMARY KEY,
+	author 		TEXT,
+	date_posted TEXT,
+	last_edited TEXT,
+	title 		TEXT,
+	filepath 	TEXT,
+	views 		INTEGER,
+	favorites	INTEGER,
+	comments		INTEGER,
+	claps 		INTEGER,
+	rocks 		INTEGER,
+	edits			INTEGER,
+	flags			INTEGER,
+	is_public	BOOLEAN,
+	is_deleted	BOOLEAN
+);
+CREATE TABLE IF NOT EXISTS comments(
+	id 			INTEGER PRIMARY KEY,
+	blog_id		INTEGER,
+	author		TEXT,
+	date_posted	TEXT,
+	last_edited TEXT,
+	claps			INTEGER,
+	rocks 		INTEGER,
+	edits			INTEGER,
+	flags			INTEGER,
+	is_deleted BOOLEAN
+);
+INSERT INTO blogs(
+	author 		,
+	date_posted ,
+	last_edited ,
+	title 		,
+	filepath 	,	
+	views 		,
+	favorites	,
+	comments		,
+	claps 		,
+	rocks 		,
+	edits			,
+	flags			,
+	is_public	,
+	is_deleted	
+)
+VALUES(
+	'Tanner Lee Woody',
+	'2019-05-29-00-00-00-000',
+	'2019-05-29-00-00-00-000',
+	'Local and Solo Github Workflow',
+	'/resources/strings/blogs/DNE.html',
+	0,
+	0,
+	0,
+	1,
+	0,
+	0,
+	0,
+	TRUE,
+	FALSE
+);
