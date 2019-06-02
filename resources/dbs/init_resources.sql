2288383b50041a30bcc64f384cd1ce0f2072746b
twoody <tanner.l.woody@gmail.com>
2019-06-02 13:06:03 -0700

    Bug with blogs table and filepath;
    filepath is set in the get_blogs.php code (serverside);

--- resources/dbs/init_resources.sql
+++ resources/dbs/init_resources.sql
@@ -50,7 +50,7 @@ VALUES(
 	'2019-05-29-00-00-00-000',
 	'2019-05-29-00-00-00-000',
 	'Local and Solo Github Workflow',
-	'/resources/strings/blogs/DNE.html',
+	'typical-git-process.html',
 	0,
 	0,
 	0,
