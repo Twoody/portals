782882a155b9329cbfc442f57c7fc0278c71d3ff
twoody <tanner.l.woody@gmail.com>
2019-06-02 23:01:21 -0700

    Added init for test commit in github blog;

--- resources/dbs/init_resources.sql
+++ resources/dbs/init_resources.sql
@@ -20,6 +20,7 @@ CREATE TABLE IF NOT EXISTS comments(
 	id 			INTEGER PRIMARY KEY,
 	blog_id		INTEGER,
 	author		TEXT,
+	content		TEXT,
 	date_posted	TEXT,
 	last_edited TEXT,
 	claps			INTEGER,
@@ -29,36 +30,60 @@ CREATE TABLE IF NOT EXISTS comments(
 	is_deleted BOOLEAN
 );
 INSERT INTO blogs(
-	author 			,
-	date_posted 	,
-	last_edited 	,
-	title 			,
-	filepath 		,	
-	views 			,
-	favorites		,
-	comments			,
-	claps 			,
-	rocks 			,
-	edits				,
-	flags				,
-	is_public		,
-	is_deleted		,
-	is_sponsored
-)
-VALUES(
-	'Tanner Lee Woody',
-	'2019-05-29-00-00-00-000',
-	'2019-05-29-00-00-00-000',
-	'Local and Solo Github Workflow',
-	'typical-git-process.html',
-	0,
-	0,
-	0,
-	1,
-	0,
-	0,
-	0,
-	TRUE,
-	FALSE,
-	FALSE
+		author 			,
+		date_posted 	,
+		last_edited 	,
+		title 			,
+		filepath 		,	
+		views 			,
+		favorites		,
+		comments			,
+		claps 			,
+		rocks 			,
+		edits				,
+		flags				,
+		is_public		,
+		is_deleted		,
+		is_sponsored
+	)
+	VALUES(
+		'Tanner Lee Woody',
+		'2019-05-29-00-00-00-000',
+		'2019-05-29-00-00-00-000',
+		'Local and Solo Github Workflow',
+		'typical-git-process.html',
+		0,
+		0,
+		0,
+		1,
+		0,
+		0,
+		0,
+		TRUE,
+		FALSE,
+		FALSE
+);
+INSERT INTO comments(
+	blog_id		,
+	author		,
+	content		,
+	date_posted	,
+	last_edited ,
+	claps			,
+	rocks 		,
+	edits			,
+	flags			,
+	is_deleted	
+	)
+	VALUES(
+		1,
+		"Tanner Lee Woody"			,
+		"Just a short comment."		,
+		"2019-05-29-00-00-00-000"	,
+		"2019-05-29-00-00-00-000"	,
+		0,
+		0,
+		0,
+		0,
+		FALSE
 );
