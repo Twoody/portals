bea01f11ae0da62af55900acefca02864a6608bf
twoody <tanner.l.woody@gmail.com>
2019-06-08 17:02:41 -0700

    Moved errors.json to errors.php because I am lazy;

---
+++ resources/strings/errors.php
@@ -0,0 +1,15 @@
+<?php
+echo "\n<!-- RESOURCE FILE: ".$PATHS['STRINGS_PHP']." -->\n";
+function get_config_strings($CONFIG=Null){
+	if ($CONFIG === Null){
+		$ROOT = ".";
+		require_once($ROOT . '/config/paths.php');
+		$PATHS	= get_paths($ROOT);
+		require_once($PATHS['SETTINGS_PATH']);
+		$CONFIG	= get_config();
+	}
+	$ROOT		= $CONFIG['ROOT'];
+	$PATHS	= get_paths($ROOT);
+
+	return Array(
+?>
