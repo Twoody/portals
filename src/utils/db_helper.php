36175a072ab4ebdcdd4d21449caba740a8aec499
twoody <tanner.l.woody@gmail.com>
2019-06-11 11:59:01 -0700

    page_views and page table init functions;
    +function add_page_to_pages($page_path, $CONFIG)
    +function process_page_views($page_path, $CONFIG)
    +function get_page_views($page_path, $CONFIG)
    +function add_page_view($page_path, $CONFIG)

--- src/utils/db_helper.php
+++ src/utils/db_helper.php
@@ -29,6 +29,111 @@ require_once($PATHS['LIBPATH_HTML']);
 require_once($PATHS['LIBPATH_FA']);
 echo "\n<!-- " . $PATHS['LIBPATH_DB_HELPER'] . " imported -->\n";
 
+function add_page_to_pages($page_path, $CONFIG){
+	$dbpath		= $CONFIG['DBPATH_RESOURCES'];
+	$table		= $CONFIG['DBTABLE_PAGES'];
+	$cur_views	= get_page_views($page_path, $CONFIG);
+	$insert		= "INSERT INTO " . $table . " (views, title, path)";
+	$insert		.= " VALUES(:views, :title, :path);";
+	$ret			= FALSE;
+	try{
+		$db		= new SQLite3($dbpath);
+		$prepare = $db->prepare($insert);
+		$prepare->bindValue(':views'	,0);
+		$prepare->bindValue(':title'	,$CONFIG['TITLE']);
+		$prepare->bindValue(':path'	,$page_path);
+		$result	= $prepare->execute();
+		if(!$result){
+			$msg	= "FAILED TO INSERT PAGE INTO PAGES TABLE `".$page_path."`";
+			update_errors_db($msg, $CONFIG);
+		}
+		else
+			$ret = TRUE;
+		$db->close();
+	}
+	catch(Exception $exception){
+		$msg	= "BAD SQL WHILE INSERTING PAGE INTO PAGES TABLE `".$page_path."`";
+		update_errors_db($msg, $CONFIG);
+	}
+	return $ret;
+}
+function process_page_views($page_path, $CONFIG){
+	$cur_views	= get_page_views($page_path, $CONFIG);
+	if ($cur_views === -1){
+		$msg	= "*** PAGE NOT FOUND AT `".$page_path."`: ";
+		$msg	.= "WILL CREATE ENTRY IN DB;";
+		update_errors_db($msg, $CONFIG);
+		if (add_page_to_pages($page_path, $CONFIG) === FALSE)
+			return -1;
+		return 1;
+	}
+	add_page_view($page_path, $CONFIG);
+	return $cur_views+1;
+	
+}
+function get_page_views($page_path, $CONFIG){
+	$dbpath		= $CONFIG['DBPATH_RESOURCES'];
+	$table		= $CONFIG['DBTABLE_PAGES'];
+	$select		= "SELECT views FROM " . $table . " WHERE path = :path";
+	$ret			= -1;
+	$error_msg	= "";
+	try{
+		$db		= new SQLite3($dbpath);
+		$prepare	= $db->prepare($select);
+		$prepare->bindValue(':path'	, $page_path );
+		$result	= $prepare->execute();
+		if($result){
+			$row	= $result->fetchArray(SQLITE3_ASSOC);
+			if ($row){
+				$ret 			= $row['views'];
+			}
+			else{
+				$error_msg	.= "FAILED TO GET ROW for get_page_views AT PATH `";
+				$error_msg	.= $page_path . "`; ";
+			}
+		}
+		else{
+			$error_msg	.= "FAILED TO GET  PAGEVIEW AT PATH `".$page_path."`";
+		}
+		$db->close();
+		if($error_msg !== '')
+			update_errors_db($error_msg, $CONFIG);
+	}
+	catch(Exception $exception){
+		$msg	= "BAD SELECT TO  FIND PAGEVIEWS AT PATH `".$page_path."`";
+		update_errors_db($msg, $CONFIG);
+	}
+	return $ret;
+}
+function add_page_view($page_path, $CONFIG){
+	$dbpath		= $CONFIG['DBPATH_RESOURCES'];
+	$table		= $CONFIG['DBTABLE_PAGES'];
+	$cur_views	= get_page_views($page_path, $CONFIG);
+	$update		= "UPDATE " . $table . " SET views = :views WHERE path = :path";
+
+	//$insert		= 'INSERT INTO ' . $table . " (message, userid, date_reported)";
+	//$insert		.= " VALUES(:msg, :userid, :date)";
+	try{
+		$db		= new SQLite3($dbpath);
+		$prepare	= $db->prepare($update);
+		$prepare->bindValue(':views'	,$cur_views+1 );
+		$prepare->bindValue(':path'	, $page_path );
+		$result	= $prepare->execute();
+		if($result)
+			$ret = TRUE;
+		else{
+			$msg	= "CANNOT ADD TO PAGEVIEW AT PATH `".$page_path."`";
+			update_errors_db($msg, $CONFIG);
+		}
+		$db->close();
+	}
+	catch(Exception $exception){
+		$msg	= "BAD UPDATE TO  ADD TO PAGEVIEW AT PATH `".$page_path."`";
+		update_errors_db($msg, $CONFIG);
+	}
+	return $ret;
+
+}
 function delete_row($table, $where, $CONFIG=Null){
 	if ($CONFIG===Null){
 		$ROOT = '.';
@@ -600,6 +705,7 @@ function update_errors_db($msg, $CONFIG, $userid=""){
 		if($result)
 			$ret = TRUE;
 		$db->close();
+		unset($db);
 	}
 	catch(Exception $exception){
 		if (!$FLAGS['is_quite'])
