671120d6c4cf1c39ca829dcd217fbb385845acb8
twoody <tanner.l.woody@gmail.com>
2019-07-07 15:45:41 -0700

    Working example of a ball with only one path;
    Ball goes from side to side depending on it's trajectory;
    Need to implement a better `bound` and updateCoord such that
    	we verify that the distance from the center of the circle
    	to any side is not less than the readius, and such that
    	point-line dist between the circle-center and the edges
    	is bigger than the radius, and such that
    	the circle center is within the canvas;

--- react/tests/ball-pen/ball_pen.js
+++ react/tests/ball-pen/ball_pen.js
@@ -2,11 +2,110 @@
 
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
-
 function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
 
 function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+var Ball = function () {
+	function Ball(props) {
+		_classCallCheck(this, Ball);
+
+		this.xCord = 21;
+		this.yCord = 21;
+		this.radius = 20;
+		this.trajectory = 120; //In degrees; 0 to 359
+		this.acceleration = 0;
+		this.color = "white";
+		this.ballId = props.ballId;
+		this.canvas = props.canvas;
+	}
+
+	_createClass(Ball, [{
+		key: 'draw',
+		value: function draw() {
+			var ctx = this.canvas.getContext('2d');
+			ctx.beginPath();
+			ctx.arc(this.xCord, this.yCord, this.radius, 2 * Math.PI, false);
+			ctx.fillStyle = "blue";
+			ctx.fill();
+		}
+	}, {
+		key: 'updateTrajectory',
+		value: function updateTrajectory(penHeight, penWidth) {
+			//Will need to update to allow for multiple heights and widths
+			//	and then find the best/only solution;
+			var xBound1 = this.xCord + this.radius;
+			var xBound2 = this.xCord - this.radius;
+			var yBound1 = this.yCord + this.radius;
+			var yBound2 = this.yCord - this.radius;
+			if (xBound1 >= penWidth || xBound2 <= 0) {
+				this.trajectory += 180;
+				if (xBound2 < 0) this.xCord = 0 + this.radius + 1;else if (xBound1 > penWidth) this.xCord = penWidth - this.radius - 1;
+			} else if (yBound1 >= penHeight || yBound2 <= 0) {
+				//	if (this.yCord >= penHeight || this.yCord <= 0){
+				this.trajectory += 180;
+				if (yBound2 < 0) this.yCord = 0 + this.radius + 1;else if (yBound1 > penHeight) this.yCord = penHeight - this.radius - 1;
+			}
+			if (this.trajectory >= 360) this.trajectory -= 360; //reset back between 0 and 359;
+		}
+	}, {
+		key: 'updateCoordinates',
+		value: function updateCoordinates() {
+			if (this.trajectory >= 330 && this.trajectory < 360) {
+				//1
+				//up one;
+				this.yCord = this.yCord - 1;
+				this.xCord = this.xCord;
+			} else if (this.trajectory >= 0 && this.trajectory < 15) {
+				//1
+				//up one;
+				this.yCord = this.yCord - 1;
+				this.xCord = this.xCord;
+			} else if (this.trajectory >= 15 && this.trajectory < 60) {
+				//2
+				//up one; right one;
+				this.yCord = this.yCord - 1;
+				this.xCord = this.xCord + 1;
+			} else if (this.trajectory >= 60 && this.trajectory < 105) {
+				//3
+				//right one;
+				this.yCord = this.yCord;
+				this.xCord = this.xCord + 1;
+			} else if (this.trajectory >= 105 && this.trajectory < 150) {
+				//4
+				//down one; right one;
+				this.yCord = this.yCord + 1;
+				this.xCord = this.xCord + 1;
+			} else if (this.trajectory >= 150 && this.trajectory < 195) {
+				//5
+				//down one;
+				this.yCord = this.yCord + 1;
+				this.xCord = this.xCord;
+			} else if (this.trajectory >= 195 && this.trajectory < 240) {
+				//6
+				//down one; left one;
+				this.yCord = this.yCord + 1;
+				this.xCord = this.xCord - 1;
+			} else if (this.trajectory >= 240 && this.trajectory < 285) {
+				//7
+				//left one;
+				this.yCord = this.yCord;
+				this.xCord = this.xCord - 1;
+			} else if (this.trajectory >= 285 && this.trajectory < 330) {
+				//8
+				//up one; left one;
+				this.yCord = this.yCord - 1;
+				this.xCord = this.xCord - 1;
+			} else {
+				console.log('BALL TRAJECTORY OUT OF BOUNDS');
+			}
+		}
+	}]);
+
+	return Ball;
+}();
 
 var BallPen = function (_React$Component) {
 	_inherits(BallPen, _React$Component);
@@ -20,6 +119,8 @@ var BallPen = function (_React$Component) {
 			height: 0,
 			width: 0
 		};
+		_this.ball = null;
+		_this.isStarted = false;
 		_this.updateWindowDimensions = _this.updateWindowDimensions.bind(_this);
 		return _this;
 	}
@@ -27,19 +128,25 @@ var BallPen = function (_React$Component) {
 	_createClass(BallPen, [{
 		key: 'componentDidMount',
 		value: function componentDidMount() {
+			var _this2 = this;
+
 			this.updateWindowDimensions();
 			this.updateCanvas();
+			this.timerID = setInterval(function () {
+				return _this2.updateCanvas();
+			}, 10);
 			window.addEventListener('resize', this.updateWindowDimensions);
+		}
+	}, {
+		key: 'componentWillUnmount',
+		value: function componentWillUnmount() {
+			clearInterval(this.timerID);
+			window.removeEventListener('resize', this.updateWindowDimensions);
 		}
 	}, {
 		key: 'componentDidUpdate',
 		value: function componentDidUpdate() {
 			this.updateCanvas();
-		}
-	}, {
-		key: 'componentWillUnmount',
-		value: function componentWillUnmount() {
-			window.removeEventListener('resize', this.updateWindowDimensions);
 		}
 	}, {
 		key: 'updateWindowDimensions',
@@ -48,7 +155,7 @@ var BallPen = function (_React$Component) {
 			if (width && width > 575) width -= 320; //Buffer for not x-small
 			else width -= 120; //Buffer for x-small
 			var height = window.innerHeight;
-			height -= 320; //Buffer...
+			height -= 280; //Buffer...
 			if (height < 0) height = 0;
 			this.setState({
 				width: width,
@@ -62,10 +169,21 @@ var BallPen = function (_React$Component) {
 			var canvas = this.refs.canvas;
 			var ctx = canvas.getContext('2d');
 			ctx.beginPath();
-			ctx.fillStyle = "#FF0000";
-			ctx.fillRect(0, 0, 100, 100);
 			ctx.rect(0, 0, this.state.width, this.state.height);
+			ctx.fillStyle = "#FF0000";
 			ctx.fill();
+			if (this.isStarted === false) {
+				//init balls
+				this.ball = new Ball({ canvas: canvas, ballID: "ball0" });
+				this.ball.draw();
+				this.isStarted = true;
+			} else {
+				//animate balls
+				//Will need to refigure trajectory and acceleration;
+				this.ball.updateTrajectory(this.state.height, this.state.width);
+				this.ball.updateCoordinates();
+				this.ball.draw();
+			}
 		}
 	}, {
 		key: 'render',
--- react/tests/ball-pen/src/ball_pen.js
+++ react/tests/ball-pen/src/ball_pen.js
@@ -1,4 +1,105 @@
 'use strict';
+class Ball{
+	constructor(props){
+		this.xCord			= 21;
+		this.yCord			= 21;
+		this.radius			= 20;
+		this.trajectory	= 120;	//In degrees; 0 to 359
+		this.acceleration	= 0;
+		this.color			= "white";
+		this.ballId			= props.ballId;
+		this.canvas			= props.canvas;
+	}
+	draw(){
+		const ctx		= this.canvas.getContext('2d');
+		ctx.beginPath();
+		ctx.arc(
+			this.xCord,
+			this.yCord, 
+			this.radius,
+			2*Math.PI,
+			false
+		);
+		ctx.fillStyle = "blue";
+		ctx.fill();
+	}
+	updateTrajectory(penHeight, penWidth){
+		//Will need to update to allow for multiple heights and widths
+		//	and then find the best/only solution;
+		const xBound1	= this.xCord + this.radius;
+		const xBound2	= this.xCord - this.radius;
+		const yBound1	= this.yCord + this.radius;
+		const yBound2	= this.yCord - this.radius;
+		if (xBound1 >= penWidth || xBound2 <= 0){
+			this.trajectory += 180;
+			if (xBound2 < 0)
+				this.xCord	= 0 + this.radius + 1;
+			else if (xBound1 > penWidth)
+				this.xCord	= penWidth - this.radius -1;
+		}
+		else if (yBound1 >= penHeight || yBound2 <= 0){
+	//	if (this.yCord >= penHeight || this.yCord <= 0){
+			this.trajectory += 180;
+			if (yBound2 < 0)
+				this.yCord	= 0 + this.radius + 1;
+			else if (yBound1 > penHeight)
+				this.yCord	= penHeight - this.radius - 1;
+		}
+		if(this.trajectory >=360)
+			this.trajectory -= 360;	//reset back between 0 and 359;
+	}
+	updateCoordinates(){
+		if (this.trajectory >= 330 && this.trajectory <360){			//1
+			//up one;
+			this.yCord = this.yCord - 1;
+			this.xCord = this.xCord;
+		}
+		else if (this.trajectory >= 0 && this.trajectory <15){			//1
+			//up one;
+			this.yCord = this.yCord - 1;
+			this.xCord = this.xCord;
+		}
+
+		else if (this.trajectory >= 15 && this.trajectory <60){		//2
+			//up one; right one;
+			this.yCord = this.yCord - 1;
+			this.xCord = this.xCord + 1;
+		}
+		else if (this.trajectory >= 60 && this.trajectory <105){		//3
+			//right one;
+			this.yCord = this.yCord;
+			this.xCord = this.xCord + 1;
+		}
+		else if (this.trajectory >= 105 && this.trajectory <150){	//4
+			//down one; right one;
+			this.yCord = this.yCord + 1;
+			this.xCord = this.xCord + 1;
+		}
+		else if (this.trajectory >= 150 && this.trajectory <195){	//5
+			//down one;
+			this.yCord = this.yCord + 1;
+			this.xCord = this.xCord;
+		}
+		else if (this.trajectory >= 195 && this.trajectory <240){	//6
+			//down one; left one;
+			this.yCord = this.yCord + 1;
+			this.xCord = this.xCord - 1;
+		}
+		else if (this.trajectory >= 240 && this.trajectory <285){	//7
+			//left one;
+			this.yCord = this.yCord;
+			this.xCord = this.xCord - 1;
+		}
+		else if (this.trajectory >= 285 && this.trajectory <330){	//8
+			//up one; left one;
+			this.yCord = this.yCord - 1;
+			this.xCord = this.xCord - 1;
+		}
+		else{
+			console.log('BALL TRAJECTORY OUT OF BOUNDS');
+		}
+	}
+}
 class BallPen extends React.Component{
 	constructor(props){
 		super(props);
@@ -6,19 +107,26 @@ class BallPen extends React.Component{
 			height: 0,
 			width: 0,
 		};
+		this.ball		= null;
+		this.isStarted	= false;
 		this.updateWindowDimensions = this.updateWindowDimensions.bind(this);
 	}
 	
 	componentDidMount() {
 		this.updateWindowDimensions();
 		this.updateCanvas();
+		this.timerID	= setInterval(
+			()=>this.updateCanvas(),
+			10
+		);
 		window.addEventListener('resize', this.updateWindowDimensions);
+	}
+	componentWillUnmount(){
+		clearInterval(this.timerID);
+		window.removeEventListener('resize', this.updateWindowDimensions);
 	}
 	componentDidUpdate() {
 		this.updateCanvas();
-	}
-	componentWillUnmount() {
-		window.removeEventListener('resize', this.updateWindowDimensions);
 	}
 
 	updateWindowDimensions() {
@@ -28,7 +136,7 @@ class BallPen extends React.Component{
 		else
 			width -= 120;	//Buffer for x-small
 		let height	= window.innerHeight;
-		height	-= 320;	//Buffer...
+		height	-= 280;	//Buffer...
 		if (height < 0)
 			height = 0;
 		this.setState({
@@ -41,10 +149,22 @@ class BallPen extends React.Component{
 		const canvas	= this.refs.canvas;
 		const ctx		= canvas.getContext('2d');
 		ctx.beginPath();
-		ctx.fillStyle = "#FF0000";
-		ctx.fillRect(0,0, 100, 100);
 		ctx.rect(0,0, this.state.width, this.state.height);
+		ctx.fillStyle = "#FF0000";
 		ctx.fill();
+		if (this.isStarted === false){
+			//init balls
+			this.ball = new Ball({canvas: canvas, ballID: "ball0"});
+			this.ball.draw();
+			this.isStarted = true;
+		}
+		else{
+			//animate balls
+			//Will need to refigure trajectory and acceleration;
+			this.ball.updateTrajectory(this.state.height, this.state.width);
+			this.ball.updateCoordinates();
+			this.ball.draw();
+		}
 	}
 
 	render(){
