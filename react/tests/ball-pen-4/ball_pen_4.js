075473ef8552fc834546b3854463998df160f9e5
twoody <tanner.l.woody@gmail.com>
2019-07-15 08:52:21 -0700

    ball-pen-4/ init;
    Plan is to apply gravity and stop the ball on this iteration;

---
+++ react/tests/ball-pen-4/ball_pen_4.js
@@ -0,0 +1,233 @@
+'use strict';
+
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+var Ball = function () {
+	function Ball(properties) {
+		_classCallCheck(this, Ball);
+
+		this.canvas = properties.canvas;
+		this.ballID = properties.ballID;
+		this.xCord = properties.xCord;
+		this.yCord = properties.yCord;
+		this.radius = properties.radius;
+		this.dx = properties.dx;
+		this.dy = properties.dy;
+		this.color = "blue";
+		this.nextX = this.xCord + this.dx;
+		this.nextY = this.yCord + this.dy;
+	}
+
+	_createClass(Ball, [{
+		key: 'draw',
+		value: function draw() {
+			var ctx = this.canvas.getContext('2d');
+			ctx.beginPath();
+			ctx.arc(this.xCord, this.yCord, this.radius, 2 * Math.PI, //Start angle in radians
+			0 //End angle in radians
+			);
+			ctx.fillStyle = this.color;
+			ctx.fill();
+		}
+	}, {
+		key: 'updateCoordinates',
+		value: function updateCoordinates() {
+			this.xCord = this.nextX;
+			this.yCord = this.nextY;
+		}
+	}, {
+		key: 'handleWallCollisions',
+		value: function handleWallCollisions(maxWidth, maxHeight) {
+			var willOverlapBottom = this.hitBottom(maxHeight);
+			var willOverlapTop = this.hitTop();
+			var willOverlapRight = this.hitRight(maxWidth);
+			var willOverlapLeft = this.hitLeft();
+			if (willOverlapTop && willOverlapBottom) {
+				//The screen is now to small for our ball;
+				//We will just keep the ball at it's current place and stop all movemnt;
+				this.nextX = this.xCord;
+				this.nextY = this.yCord;
+				this.dy = 0;
+				this.dx = 0;
+				console.log('WARNING: SCREEN NOT FITTED;');
+			} else if (willOverlapBottom) {
+				this.dy *= -1;
+				this.nextY = maxHeight - this.radius;
+			} else if (willOverlapTop) {
+				this.dy *= -1;
+				this.nextY = 0 + this.radius;
+			} else {
+				//No collision
+			}
+			if (willOverlapRight && willOverlapLeft) {
+				//The screen is now to small for our ball;
+				//We will just keep the ball at it's current place and stop all movemnt;
+				this.nextX = this.xCord;
+				this.nextY = this.yCord;
+				this.dy = 0;
+				this.dx = 0;
+				console.log('WARNING: SCREEN NOT FITTED;');
+			} else if (willOverlapRight) {
+				this.dx *= -1;
+				this.nextX = maxWidth - this.radius;
+			} else if (willOverlapLeft) {
+				this.dx *= -1;
+				this.nextX = 0 + this.radius;
+			} else {
+				//No collision
+			}
+		}
+	}, {
+		key: 'hitBottom',
+		value: function hitBottom(maxHeight) {
+			var ballMaxBottom = this.nextY + this.radius;
+			if (ballMaxBottom >= maxHeight) return true;
+			return false;
+		}
+	}, {
+		key: 'hitTop',
+		value: function hitTop() {
+			var ballMaxTop = this.nextY - this.radius;
+			if (ballMaxTop <= 0) return true;
+			return false;
+		}
+	}, {
+		key: 'hitRight',
+		value: function hitRight(maxWidth) {
+			var ballMaxRight = this.nextX + this.radius;
+			if (ballMaxRight >= maxWidth) return true;
+			return false;
+		}
+	}, {
+		key: 'hitLeft',
+		value: function hitLeft() {
+			var ballMaxLeft = this.nextX - this.radius;
+			if (ballMaxLeft <= 0) return true;
+			return false;
+		}
+	}]);
+
+	return Ball;
+}(); //End Ball Class
+
+
+var BallPen = function (_React$Component) {
+	_inherits(BallPen, _React$Component);
+
+	function BallPen(props) {
+		_classCallCheck(this, BallPen);
+
+		var _this = _possibleConstructorReturn(this, (BallPen.__proto__ || Object.getPrototypeOf(BallPen)).call(this, props));
+
+		_this.state = {
+			height: 0,
+			width: 0
+		};
+		_this.balls = [];
+		_this.updateWindowDimensions = _this.updateWindowDimensions.bind(_this);
+		return _this;
+	}
+
+	_createClass(BallPen, [{
+		key: 'componentDidMount',
+		value: function componentDidMount() {
+			var _this2 = this;
+
+			this.updateWindowDimensions();
+			this.updateCanvas();
+			this.timerID = setInterval(function () {
+				return _this2.updateCanvas();
+			}, 25);
+			window.addEventListener('resize', this.updateWindowDimensions);
+		}
+	}, {
+		key: 'componentWillUnmount',
+		value: function componentWillUnmount() {
+			clearInterval(this.timerID);
+			window.removeEventListener('resize', this.updateWindowDimensions);
+		}
+	}, {
+		key: 'componentDidUpdate',
+		value: function componentDidUpdate() {
+			this.updateCanvas();
+		}
+	}, {
+		key: 'updateWindowDimensions',
+		value: function updateWindowDimensions() {
+			var width = window.innerWidth;
+			if (width && width > 575) width -= 320; //Buffer for not x-small
+			else width -= 120; //Buffer for x-small
+			var height = window.innerHeight;
+			height -= 280; //Buffer...
+			if (height < 0) height = 0;
+			this.setState({
+				width: width,
+				height: height
+			});
+			return;
+		}
+	}, {
+		key: 'updateCanvas',
+		value: function updateCanvas() {
+			var canvas = this.canvasRef;
+			var ctx = canvas.getContext('2d');
+			ctx.beginPath();
+			ctx.rect(0, 0, this.state.width, this.state.height);
+			ctx.fillStyle = "#FF0000";
+			ctx.fill();
+			if (this.state.width !== 0) {
+				if (this.balls.length === 0) {
+					// Init first ball
+					this.balls.push(new Ball({
+						canvas: canvas,
+						ballID: 0,
+						xCord: 41,
+						yCord: 41,
+						radius: 30,
+						dx: 2,
+						dy: 2
+					}));
+				} // End first ball init;
+				for (var i = 0; i < this.balls.length; i++) {
+					var ball = this.balls[i];
+					ball.nextX = ball.xCord + ball.dx;
+					ball.nextY = ball.yCord + ball.dy;
+					ball.handleWallCollisions(this.state.width, this.state.height);
+					ball.updateCoordinates();
+					ball.draw();
+				} //end i-for
+			}
+		}
+	}, {
+		key: 'render',
+		value: function render() {
+			var _this3 = this;
+
+			var penStyle = {
+				border: "1px solid #000000"
+			};
+			return React.createElement(
+				'div',
+				null,
+				React.createElement('canvas', {
+					ref: function ref(canvas) {
+						return _this3.canvasRef = canvas;
+					},
+					width: this.state.width,
+					height: this.state.height,
+					style: penStyle
+				})
+			);
+		}
+	}]);
+
+	return BallPen;
+}(React.Component);
+
+ReactDOM.render(React.createElement(BallPen, null), document.getElementById('ball-pen-4'));
---
+++ react/tests/ball-pen-4/src/ball_pen_4.js
@@ -0,0 +1,203 @@
+'use strict';
+class Ball{
+	constructor(properties){
+		this.canvas	= properties.canvas;
+		this.ballID	= properties.ballID;
+		this.xCord	= properties.xCord;
+		this.yCord	= properties.yCord;
+		this.radius	= properties.radius;
+		this.dx 		= properties.dx;
+		this.dy		= properties.dy;
+		this.color	= "blue";
+		this.nextX	= this.xCord + this.dx;
+		this.nextY	= this.yCord + this.dy;
+	}
+	draw(){
+		const ctx = this.canvas.getContext('2d');
+		ctx.beginPath();
+		ctx.arc(
+			this.xCord,
+			this.yCord, 
+			this.radius,
+			2*Math.PI,		//Start angle in radians
+			0					//End angle in radians
+		);
+		ctx.fillStyle = this.color;
+		ctx.fill();
+	}
+	updateCoordinates(){
+		this.xCord = this.nextX;
+		this.yCord = this.nextY;
+	}
+	handleWallCollisions(maxWidth, maxHeight){
+		const willOverlapBottom	= this.hitBottom(maxHeight);
+		const willOverlapTop		= this.hitTop();
+		const willOverlapRight	= this.hitRight(maxWidth);
+		const willOverlapLeft	= this.hitLeft();
+		if(willOverlapTop && willOverlapBottom){
+			//The screen is now to small for our ball;
+			//We will just keep the ball at it's current place and stop all movemnt;
+			this.nextX = this.xCord;
+			this.nextY = this.yCord;
+			this.dy = 0;
+			this.dx = 0;
+			console.log('WARNING: SCREEN NOT FITTED;');
+		}
+		else if(willOverlapBottom){
+			this.dy *= -1;
+			this.nextY = maxHeight - this.radius;
+		}
+		else if(willOverlapTop){
+			this.dy *= -1;
+			this.nextY = 0 + this.radius;
+		}
+		else{
+			//No collision
+		}
+		if(willOverlapRight && willOverlapLeft){
+			//The screen is now to small for our ball;
+			//We will just keep the ball at it's current place and stop all movemnt;
+			this.nextX = this.xCord;
+			this.nextY = this.yCord;
+			this.dy = 0;
+			this.dx = 0;
+			console.log('WARNING: SCREEN NOT FITTED;');
+		}
+		else if(willOverlapRight){
+			this.dx *= -1;
+			this.nextX = maxWidth - this.radius;
+		}
+		else if(willOverlapLeft){
+			this.dx *= -1;
+			this.nextX = 0 + this.radius;
+		}
+		else{
+			//No collision
+		}
+
+	}
+	hitBottom(maxHeight){
+		const ballMaxBottom = this.nextY + this.radius;
+		if(ballMaxBottom >= maxHeight)
+			return true;
+		return false;
+	
+	}
+	hitTop(){
+		const ballMaxTop = this.nextY - this.radius;
+		if(ballMaxTop <= 0)
+			return true;
+		return false;
+	}
+	hitRight(maxWidth){
+		const ballMaxRight = this.nextX + this.radius;
+		if(ballMaxRight >= maxWidth)
+			return true;
+		return false;
+	
+	}
+	hitLeft(){
+		const ballMaxLeft = this.nextX - this.radius;
+		if(ballMaxLeft <= 0)
+			return true;
+		return false;
+	}
+}//End Ball Class
+class BallPen extends React.Component{
+   constructor(props){
+      super(props);
+      this.state      = {
+         height: 0,
+         width:  0,
+      };
+		this.balls	= [];
+      this.updateWindowDimensions = this.updateWindowDimensions.bind(this);
+   }
+   
+   componentDidMount() {
+      this.updateWindowDimensions();
+      this.updateCanvas();
+      this.timerID   = setInterval(
+         ()=>this.updateCanvas(),
+         25
+      );
+      window.addEventListener('resize', this.updateWindowDimensions);
+   }
+   componentWillUnmount(){
+      clearInterval(this.timerID);
+      window.removeEventListener('resize', this.updateWindowDimensions);
+   }
+   componentDidUpdate() {
+      this.updateCanvas();
+   }
+
+   updateWindowDimensions() {
+      let width   = window.innerWidth;
+      if (width && width >575)
+         width -= 320;   //Buffer for not x-small
+      else
+         width -= 120;   //Buffer for x-small
+      let height   = window.innerHeight;
+      height   -= 280;   //Buffer...
+      if (height < 0)
+         height = 0;
+      this.setState({
+         width: width, 
+         height: height
+      });
+      return;
+   }
+   updateCanvas(){
+      const canvas   = this.canvasRef;
+      const ctx      = canvas.getContext('2d');
+      ctx.beginPath();
+      ctx.rect(0,0, this.state.width, this.state.height);
+      ctx.fillStyle = "#FF0000";
+      ctx.fill();
+		if(this.state.width !== 0){
+			if(this.balls.length === 0){
+				// Init first ball
+				this.balls.push(
+					new Ball({
+						canvas:	canvas,
+						ballID:	0,
+						xCord:	41,
+						yCord:	41,
+						radius:	30,
+						dx: 		2,
+						dy:		2
+					})
+				);
+			}// End first ball init;
+			for(let i=0; i<this.balls.length; i++){
+				let ball	= this.balls[i];
+				ball.nextX = ball.xCord + ball.dx;
+				ball.nextY = ball.yCord + ball.dy;
+				ball.handleWallCollisions(this.state.width, this.state.height);
+				ball.updateCoordinates();
+				ball.draw();
+			}//end i-for
+		}
+   }
+
+   render(){
+      const penStyle   = {
+         border:   "1px solid #000000"
+      };
+      return (
+         <div>
+            <canvas
+               ref={canvas => this.canvasRef = canvas}
+               width={this.state.width}
+               height={this.state.height}
+               style={penStyle}
+            />
+         </div>
+      );
+   }
+}
+
+ReactDOM.render(
+	<BallPen />,
+	document.getElementById('ball-pen-4')
+);
