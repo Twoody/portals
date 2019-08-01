5b37b56074e20d04a96b6f81f845ebf809ff4ff4
twoody <tanner.l.woody@gmail.com>
2019-08-01 15:20:46 -0700

    makeRandomBall() init;
    finally found a good way to do this;

--- react/tests/rectangle-4/src/utils.js
+++ react/tests/rectangle-4/src/utils.js
@@ -16,6 +16,39 @@ function getRandomInt(min, max) {
 	max = Math.floor(max);
 	return Math.floor(Math.random() * (max - min + 1)) + min;
 }
+function makeRandomBall(sWidth, sHeight, ballID, minRadius=3, maxRadius=30, maxSpeed=29){
+	/*	Return false if random ball fails;
+		Else return random ball;
+		Input:
+			screen width,
+			screen height,
+			ballID
+			mininum radius possible,
+			maximum radius possible,
+			maxSpeed possible -- no more than 2 times given radius;
+		Output:
+			Ball object;
+		@ ./src/Balls.js
+	*/
+	let randomRadius	= getRandomInt(minRadius, maxRadius);
+	randomRadius += getRandomInt(1,99) * 0.01;
+	const randomX			= getRandomInt(randomRadius, sWidth - randomRadius);
+	const randomY			= getRandomInt(randomRadius, sHeight - randomRadius);
+	const randomDX			= getRandomInt(1, 20) * 0.01;
+	const randomDY			= getRandomInt(1, 20) * 0.01;
+	const newBall			= new Ball({
+		ballID:	ballID,
+		color:	getRandomColor(),
+		xCord:	randomX,
+		yCord:	randomY,
+		radius:	randomRadius,
+		dx: 		randomDX,
+		dy:		randomDY,
+	});
+	newBall.maxSpeed = maxSpeed;
+	return newBall;
+}//end makeRandomBall
+
 function writeToScreen(ctx, msg, x, y, color="black"){
 	ctx.beginPath();
 	ctx.font      = "25px Arial";
--- react/tests/rectangle-4/utils.js
+++ react/tests/rectangle-4/utils.js
@@ -16,6 +16,43 @@ function getRandomInt(min, max) {
 	max = Math.floor(max);
 	return Math.floor(Math.random() * (max - min + 1)) + min;
 }
+function makeRandomBall(sWidth, sHeight, ballID) {
+	var minRadius = arguments.length > 3 && arguments[3] !== undefined ? arguments[3] : 3;
+	var maxRadius = arguments.length > 4 && arguments[4] !== undefined ? arguments[4] : 30;
+	var maxSpeed = arguments.length > 5 && arguments[5] !== undefined ? arguments[5] : 29;
+
+	/*	Return false if random ball fails;
+ 	Else return random ball;
+ 	Input:
+ 		screen width,
+ 		screen height,
+ 		ballID
+ 		mininum radius possible,
+ 		maximum radius possible,
+ 		maxSpeed possible -- no more than 2 times given radius;
+ 	Output:
+ 		Ball object;
+ 	@ ./src/Balls.js
+ */
+	var randomRadius = getRandomInt(minRadius, maxRadius);
+	randomRadius += getRandomInt(1, 99) * 0.01;
+	var randomX = getRandomInt(randomRadius, sWidth - randomRadius);
+	var randomY = getRandomInt(randomRadius, sHeight - randomRadius);
+	var randomDX = getRandomInt(1, 20) * 0.01;
+	var randomDY = getRandomInt(1, 20) * 0.01;
+	var newBall = new Ball({
+		ballID: ballID,
+		color: getRandomColor(),
+		xCord: randomX,
+		yCord: randomY,
+		radius: randomRadius,
+		dx: randomDX,
+		dy: randomDY
+	});
+	newBall.maxSpeed = maxSpeed;
+	return newBall;
+} //end makeRandomBall
+
 function writeToScreen(ctx, msg, x, y) {
 	var color = arguments.length > 4 && arguments[4] !== undefined ? arguments[4] : "black";
 
