7275cec77011926b45c51997e48a9914d436b455
twoody <tanner.l.woody@gmail.com>
2019-08-09 12:16:08 -0700

    setup to resemeble /rectangle-7/index.php;
    better design and look;

--- index.php
+++ index.php
@@ -32,9 +32,9 @@ make_imports($ROOT);
 $CONFIG	= get_config($ROOT);
 $PATHS	= get_paths($ROOT);
 $STRINGS	= get_config_strings($CONFIG);
-$react_div				= "<div id=\"rectangle-6\"></div>";
-$react_dir				= $ROOT. "/react/tests/rectangle-6";
-$react_js				= $react_dir. "/rectangle_6.js";
+$react_div				= "<div id=\"rectangle-7\"></div>";
+$react_dir				= $ROOT. "/react/tests/rectangle-7";
+$react_js				= $react_dir. "/rectangle_7.js";
 $utils_js				= $react_dir. "/utils.js";
 $balls_js				= $react_dir. "/Balls.js";
 $rectangle_js			= $react_dir. "/Rectangle.js";
@@ -65,10 +65,6 @@ $CONFIG['ACTIVE_PAGE']		= $PATHS['NAV_HOME'];
 
 echo "<!-- LANDED ON: HOME PAGE-->";
 
-$ad_not_sm	= "";	//Show WHEN NOT X-SMALL
-$ad_not_sm	.= "\n\t\t\t\t\t<span class=\"d-none d-sm-block\">";
-$ad_not_sm	.= get_ad($CONFIG);
-$ad_not_sm	.= "\n\t\t\t\t\t</span>";
 $page_views	= make_page_views($CONFIG['ACTIVE_PAGE'], $CONFIG);
 $instructions_content	=	"Click on a ball to accelerate it;<br/>";
 $instructions_content	.=	"Click on an empty space to init a new ball; <br/>";
@@ -76,22 +72,17 @@ $instructions_content	.=	"New balls will only be made if there is room (e.g. ";
 $instructions_content	.=	"there are too many balls or new ball would be out of bounds)";
 $instructions	= make_tag('p', Array('content'=>$instructions_content), $CONFIG);
 
-$col0_content	= "<hr class=\"thick-line\">". $react_div . $instructions_content;
+$col0_content	= "<hr id=\"thickline0\" class=\"thick-line\">". $react_div . $instructions_content;
 
 $col0	= Array(
-	'class'=>"col-12 col-sm-8 col-md-9 col-lg-10 m-0 p-0 fit-screen",
+	'class'=>"col-12 m-0 p-0 fit-screen",
 	'content'=>$col0_content,
 );
-$col1	= Array(
-	'class'=>" col-sm-4 col-md-3 col-lg-2 pr-3 m-0",
-	'content'=>$ad_not_sm,
-);
 $col_0	= make_tag("div", $col0, $CONFIG) . "<!-- END COL -->";
-$col_1	= make_tag("div", $col1, $CONFIG) . "<!-- END COL -->";
 
 $row0	= Array(
 	'class'=>" row pl-3 pr-3 m-0",
-	'content'=>$col_0.$col_1,
+	'content'=>$col_0,
 );
 $row_0	= make_tag("div", $row0, $CONFIG) . "<!-- END ROW -->";
 $container0 =  Array(
@@ -99,10 +90,8 @@ $container0 =  Array(
 	'content'=>$row_0,
 );
 $container_0	= make_tag("div", $container0, $CONFIG);
-$container_1	= make_gen_container(get_ads_sm($CONFIG), $CONFIG);
 $body .= $page_views;
 $body .= $container_0;
-$body .= $container_1;
 
 $CONFIG['BODY'] = $body;
 echo template_b($CONFIG) . "\n";
