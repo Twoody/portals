53dc34c01ce9568aeb2b76981633629d1841698b
twoody <tanner.l.woody@gmail.com>
2019-08-09 11:49:48 -0700

    Major changes to the design;
    Removed second col (`col1`) and other mobile ads segment;
    Set header to be off;
    Set footer to be off;
    Idea is to have a more clean and professional display;

--- react/tests/rectangle-7/index.php
+++ react/tests/rectangle-7/index.php
@@ -38,7 +38,9 @@ $react_js		= "rectangle_7.js";
 
 /* ----- ----- GENERAL CHANGES BEFORE SECOND IMPORT ----- ----- */
 $CONFIG['TITLE']				= "CHANGE ME";;
-$CONFIG['DISPLAY_HEADER']	= TRUE;
+$CONFIG['DISPLAY_HEADER']	= FALSE;
+$CONFIG['DISPLAY_FOOTER']	= FALSE;
+
 $CONFIG['ACTIVE_PAGE']		= __FILE__;
 require_once($PATHS['LIBPATH_DB_PAGES']);
 require_once($PATHS['TEMPLATES_B']);
@@ -52,32 +54,23 @@ $CONFIG['CUSTOM_SCRIPTS'] .= "\n\t<script src=\"./Rectangle.js\"></script>";
 $CONFIG['CUSTOM_SCRIPTS'] .= "\n\t<script src=\"./utils.js\"></script>";
 $CONFIG['CUSTOM_SCRIPTS'] .= "\n\t<script type=\"text/babel\" src=\"".$react_js."\"></script>";
 
-$ad_not_sm	= "";	//Show WHEN NOT X-SMALL
-$ad_not_sm	.= "\n\t\t\t\t\t<span class=\"d-none d-sm-block\">";
-$ad_not_sm	.= get_ad($CONFIG);
-$ad_not_sm	.= "\n\t\t\t\t\t</span>";
 $instructions_content	=	"Click on a ball to accelerate it;<br/>";
 $instructions_content	.=	"Click on an empty space to init a new ball; <br/>";
 $instructions_content	.=	"New balls will only be made if there is room (e.g. ";
 $instructions_content	.=	"there are too many balls or new ball would be out of bounds)";
 $instructions	= make_tag('p', Array('content'=>$instructions_content), $CONFIG);
 
-$col0_content	= "<hr class=\"thick-line\">". $react_div . $instructions_content;
+$col0_content	= "<hr id=\"thickline0\" class=\"thick-line\">". $react_div . $instructions_content;
 $page_views	= make_page_views($CONFIG['ACTIVE_PAGE'], $CONFIG);
 $col0	= Array(
-	'class'=>"col-12 col-sm-8 col-md-9 col-lg-10 m-0 p-0 fit-screen",
+	'class'=>"col-12 m-0 p-0 fit-screen",
 	'content'=>$col0_content,
 );
-$col1	= Array(
-	'class'=>" col-sm-4 col-md-3 col-lg-2 pr-3 m-0",
-	'content'=>$ad_not_sm,
-);
 $col_0	= make_tag("div", $col0, $CONFIG) . "<!-- END COL -->";
-$col_1	= make_tag("div", $col1, $CONFIG) . "<!-- END COL -->";
 
 $row0	= Array(
 	'class'=>" row pl-3 pr-3 m-0",
-	'content'=>$col_0.$col_1,
+	'content'=>$col_0,
 );
 $row_0	= make_tag("div", $row0, $CONFIG) . "<!-- END ROW -->";
 $container0 =  Array(
@@ -85,10 +78,8 @@ $container0 =  Array(
 	'content'=>$row_0,
 );
 $container_0	= make_tag("div", $container0, $CONFIG);
-$container_1	= make_gen_container(get_ads_sm($CONFIG), $CONFIG);
 $body .= $page_views;
 $body .= $container_0;
-$body .= $container_1;
 
 $CONFIG['BODY'] = $body;
 echo template_b($CONFIG) . "\n";
