a4918a67cf103819f57ac964bbf3e91ad24d87e2
twoody <tanner.l.woody@gmail.com>
2019-08-21 15:25:05 -0700

    @Balls import + constructor() + initBalls() + updateRectangle()
    Importing Ball() class to make our own custom balls;
    
    constructor():
    	customizing our world to be specific to our level;
    	no branded balls,
    	only one ball,
    	gravity, friction, inertia, no trails;
    initBalls():
    	Rewrite of our parent method;
    	No random balls, just one ball ontop of our rectangle;
    	DOES NOT CALL SUPER;
    updateRectangle():
    	Calling super;
    	Utilizing a spacebar check (aka didInit flag) to nuke this.balls;
    	If we nuke balls, we then just initBalls() again to redraw and refoc our
    	centered ball;

--- react/tests/game-1/react-src/src/Level_1.js
+++ react/tests/game-1/react-src/src/Level_1.js
@@ -1,4 +1,5 @@
 import { World } from "./World.js";
+import { Ball } from "./Ball.js";
 
 export class Level1 extends World{
 	/*
@@ -8,8 +9,43 @@ export class Level1 extends World{
 		Add keycode `space` to accelerate the ball from
 			our movable rectangle;
 	*/
-	constructor(props){
+	constructor(props={}){
+		props.hasBrandBalls	= false;
+		props.hasMovableRect	= true;
+		props.initBallCnt		= 1;
 		super(props);
+		this.hasGravity		= true;
+		this.hasWallFriction = true;
+		this.hasIneritia		= true;
+		this.hasBallFriction	= true;
+		this.didInit			= false;	//Init is when we hit spacebar
+	}
+	initBalls(){
+		//WWARNING: Rewriting World.initBalls()
+		if(!this.width || this.width === 0)
+			return false;
+		const rect		= this.rectangles[0];
+		const radius	= 20;
+		const newBall	= new Ball({
+			ballID:	0,
+			color:	"yellow",
+			xCord:	rect.xCenter,
+			yCord:	rect.yTop - radius-0.001, //Buffer
+			radius:	radius,
+			dx: 		0,
+			dy:		0,
+		});
+		this.balls.push(newBall);
+		this.initBallCnt = 1;
+		return true;
+	}
+	updateRectangle(ctx){
+		//Update the ball with the rectangle while we did not init;
+		super.updateRectangle(ctx);
+		if(this.didInit === false){
+			this.balls = [];
+			this.initBalls();
+		}
 	}
 }//end Level1 Class
 
