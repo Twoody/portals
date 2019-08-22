d3161ca97e8d0c32d2e5cea3ac7685d7952462d6
twoody <tanner.l.woody@gmail.com>
2019-08-21 22:34:48 -0700

    Working example of game minus breakable rects;
    Ball sits ontop of rectangle and moves with it;
    Ball resets when ball hits bottom;
    Spacebar accelerates ball if only one ball;

--- react/tests/game-1/react-src/src/Level_1.js
+++ react/tests/game-1/react-src/src/Level_1.js
@@ -1,5 +1,6 @@
 import { World } from "./World.js";
 import { Ball } from "./Ball.js";
+import { accelerateBalls} from "./Ball.js";
 
 export class Level1 extends World{
 	/*
@@ -14,14 +15,18 @@ export class Level1 extends World{
 		props.hasMovableRect	= true;
 		props.initBallCnt		= 1;
 		super(props);
-		this.hasGravity		= true;
+		this.hasGravity		= false;
 		this.hasWallFriction = true;
 		this.hasIneritia		= true;
 		this.hasBallFriction	= true;
 		this.didInit			= false;	//Init is when we hit spacebar
+		this.reservedKeys.push(32); 	//Adding spacebar eventcode; Will remove with didInit;
 	}
 	initBalls(){
 		//WWARNING: Rewriting World.initBalls()
+		//This init should only have one ball:
+		//	Ball should be centered around our movableRect, which is
+		//	defaulted as always being made first and at rectangles[0]
 		if(!this.width || this.width === 0)
 			return false;
 		const rect		= this.rectangles[0];
@@ -35,9 +40,20 @@ export class Level1 extends World{
 			dx: 		0,
 			dy:		0,
 		});
+		newBall.maxSpeed = radius * 0.66;
 		this.balls.push(newBall);
 		this.initBallCnt = 1;
 		return true;
+	}
+	updateBalls(ctx){
+		for (let i=0; i<this.balls.length; i++){
+			let ball = this.balls[i];
+			if(ball.hitBottom(this.height)){
+				this.balls.splice(i, 1);
+				this.didInit = false;
+			}
+		}
+		super.updateBalls(ctx);
 	}
 	updateRectangle(ctx){
 		//Update the ball with the rectangle while we did not init;
@@ -47,5 +63,22 @@ export class Level1 extends World{
 			this.initBalls();
 		}
 	}
+	handleKeydown(keycode, ctx){
+		super.handleKeydown(keycode, ctx);
+		if( this.didInit === false && keycode === 32){
+			this.didInit = true;
+			for( let i=0; i< this.balls.length; i++){
+				let ball = this.balls[i];
+				ball.accelerate(10,10);
+			}
+			/*
+			const spaceKeyIndex = this.reservedKeys.indexOf(32);
+			if(spaceKeyIndex > -1){
+				this.reservedKeys.splice(spaceKeyIndex, 1);
+			}
+			*/
+		}
+	}
+
 }//end Level1 Class
 
