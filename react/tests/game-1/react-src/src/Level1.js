810bdaa20341bf2cc8e54dba2f1690b427ff68b9
twoody <tanner.l.woody@gmail.com>
2019-08-23 23:38:10 -0700

    Broken up s.t. Level* pulls from Level.js
    We basically only need to declare the `makeDestructibles()` method
    in each level now;

---
+++ react/tests/game-1/react-src/src/Level1.js
@@ -0,0 +1,36 @@
+import { Level } from "./Level.js";
+import { Rectangle } from "./Rectangle.js";
+import { getMiddleOfCanvas } from "./utils.js";
+
+export class Level1 extends Level{
+	/*
+		Try to build a world with a single ball;
+		Build the world with on movable rectangle;
+		Build the world with one destructable rectangle;
+		Add keycode `space` to accelerate the ball from
+			our movable rectangle;
+		IFF ball hits bottom and no more balls, reset game;
+	*/
+	constructor(props={}){
+		super(props);
+	}
+	makeDestructibleRects(){
+		const middleCords	= getMiddleOfCanvas(this.width, this.height);
+		const width			= this.brickWidth;
+		const height		= this.brickHeight;
+		const xLeft			= middleCords.x - width/2;
+		const yTop			= middleCords.y - height/2 - 100;
+		const rectangle	= new Rectangle({
+			rectID:	this.rectangles.length,
+			color:	'white',
+			xLeft:	xLeft,
+			yTop:		yTop,
+			width:	width,
+			height:	height,
+		});
+		rectangle.isDestructible	= true;
+		rectangle.isDraggable		= false;
+		this.rectangles.push(rectangle);
+	}
+}//end Level1 Class
+
---
+++ react/tests/game-1/react-src/src/Level2.js
@@ -0,0 +1,38 @@
+import { Level } from "./Level.js";
+import { Rectangle } from "./Rectangle.js";
+import { getMiddleOfCanvas } from "./utils.js";
+
+export class Level2 extends Level{
+	/*
+		Try to build a world with a single ball;
+		Build the world with on movable rectangle;
+		Build the world with one destructable rectangle;
+		Add keycode `space` to accelerate the ball from
+			our movable rectangle;
+		IFF ball hits bottom and no more balls, reset game;
+	*/
+	constructor(props={}){
+		super(props);
+	}
+	makeDestructibleRects(){
+		const middleCords		= getMiddleOfCanvas(this.width, this.height);
+		const yTop				= middleCords.y - Math.floor(this.height/4);
+		const padding			= 5;
+		const possibleRects	= Math.floor(this.width / (this.brickWidth + padding*2));
+		for (let i=0; i< possibleRects; i++){
+			const bx = (this.brickWidth * i) + (padding * i+1)
+			const rectangle	= new Rectangle({
+				rectID:	this.rectangles.length,
+				color:	'white',
+				xLeft:	bx,
+				yTop:		yTop,
+				width:	this.brickWidth,
+				height:	this.brickHeight,
+			});
+			rectangle.isDestructible	= true;
+			rectangle.isDraggable		= false;
+			this.rectangles.push(rectangle);
+		}//end i-for
+		return this.rectangles.length-1;
+	}
+}//end Level2 Class
