25f488a3ac0dcd28c389470d5499253273cfcd0d
TWoody <passtowc@box5385.bluehost.com>
2019-08-29 14:36:27 -0600

    proper index.php from intToWord.php

--- precoa/index.php
+++ precoa/index.php
@@ -1,30 +1,4 @@
 <?php
-session_start();
-?>
-<!doctype html>
-<?php
-/******************************************************************************
-   Author:  Tanner.L.Woody@gmail.com
-   WebLink: 
-   Date:    2019-04-20
-
-USAGE:
-	First Check for compilation errors:
-		php index.php
-	Second, host:
-		With PHP:
-			clear & php -S localhost:8000 templates/login.php
-		With APACHE:
-			sudo apachectl start
-	Third, change ROOT to corresponding dir path;
-
-Purpose:
-	Demonstrate int to text coding question;
-
-Links:
-	https://www.w3schools.com/bootstrap4/bootstrap_carousel.asp
-	https://stackoverflow.com/questions/12172177/set-div-height-equal-to-screen-size
-******************************************************************************/
 $INT_WORD_MAP = Array(
 	0 => "zero",
 	1 => "one",
@@ -64,6 +38,8 @@ function intToText($n){
 			String of text
 	*/
 	global $INT_WORD_MAP;
+	if (is_int($n) === FALSE)
+		return "NOT GOOD ENTRY";
 	$t = "";
 	if ($n === 0){
 		$t .= $INT_WORD_MAP[0];
@@ -99,68 +75,37 @@ function intToText($n){
 	return $t;
 }//end intToText()
 
-$ROOT = '..';
-require_once($ROOT.'/config/imports.php');
-make_imports($ROOT);
-
-$CONFIG			= get_config($ROOT);
-$PATHS			= get_paths($ROOT);
-$STRINGS			= get_config_strings($CONFIG);
-$body				= "";
-
-/* ----- ----- GENERAL CHANGES BEFORE SECOND IMPORT ----- ----- */
-$CONFIG['TITLE']				= "CHANGE ME";;
-$CONFIG['DISPLAY_HEADER']	= TRUE;
-$CONFIG['ACTIVE_PAGE']		= __FILE__;
-require_once($PATHS['LIBPATH_DB_PAGES']);
-require_once($PATHS['TEMPLATES_B']);
-
-//$CONFIG['CUSTOM_STYLES'] .= "\n<style></style>";
-//$CONFIG['CUSTOM_SCRIPTS'] .= "\n<script></script>";
-
-$page_views	= make_page_views($CONFIG['ACTIVE_PAGE'], $CONFIG);
-$onclick = "var query = document.getElementById(\"qquery\");";
-$onclick .= "document.location.href = '" . $ROOT . "/precoa/index.php?' + query;";
-$input = "<input id=\"qquery\" type=\"text\" class=\"form-control\">";
-$button_arr	= Array(
-	'class' => "btn btn-default",
-	'type' => "button",
-	'onClick' => "vardocument.location.href = '" . $ROOT . "/precoa/index.php?'",
-	"content" => "Wordify"
-);
-$button = make_tag("button", $button_arr, $config);
-$span_arr = Array(
-	"class" => "input-group-btn",
-	"content" => $button,
-);
-$span_button = make_tag("span", $span_arr, $CONFIG);
-$input_content = $input . $span_button;
-$q_arr	= Array(
-	'class' => 'input-group',
-	'content'=> $input_content
-);
-
-$q_content = make_tag("div", $q_arr, $CONFIG);
-
-$col0	= Array(
-	'class'=>"col-12 m-0 p-0 fit-screen",
-	'content'=>"<hr class=\"thick-line\">" . $q_content,
-);
-$col_0	= make_tag("div", $col0, $CONFIG) . "<!-- END COL -->";
-
-$row0	= Array(
-	'class'=>" row pl-3 pr-3 m-0",
-	'content'=>$col_0.$col_1,
-);
-$row_0	= make_tag("div", $row0, $CONFIG) . "<!-- END ROW -->";
-$container0 =  Array(
-	'class'=>" container-fluid pl-3 pr-3 m-0",
-	'content'=>$row_0,
-);
-$container_0	= make_tag("div", $container0, $CONFIG);
-$body .= $page_views;
-$body .= $container_0;
-
-$CONFIG['BODY'] = $body;
-echo template_b($CONFIG) . "\n";
+if(isset($_GET['anInt'])){
+	$q = intval($_GET['anInt']);
+	$r = intToText($q);
+	echo "<p>".$r."</p>";
+}
 ?>
+<!DOCTYPE html>
+<html>
+<body>
+
+<h2>Text Input</h2>
+
+<form>
+  Integer from 0 to 9999:<br>
+  <input type="text" name="anInt">
+  <input type="submit" value="Submit">
+</form>
+
+<p>
+	Hi Brandon, thanks again for the interview today. I got the problem sketched out via php
+	and thought you would like to be able to submit the data, too.
+	The file can be viewed at:
+	<a href ="https://github.com/Twoody/portals/blob/master/precoa/intToText.php"
+		target="_blank">
+	 the git repo for this page.
+	<a/>
+</p>
+
+
+<img src="./whiteboard.jpg"
+	style="width:500px;height:600px;"
+/>
+</body>
+</html>
