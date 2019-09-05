9a9b1f2fc9209d56d96cb2198012f6cbd5b604b8
twoody <tanner.l.woody@gmail.com>
2019-09-05 14:26:26 -0700

    Level5 init;
    Not done yet;

---
+++ react/tests/game-1/react-src/src/Level5.js
@@ -0,0 +1,55 @@
+import { Level } from "./Level.js";
+import { Rectangle } from "./Rectangle.js";
+import { getMiddleOfCanvas } from "./utils.js";
+
+export class Level3 extends Level{
+	/*
+		Try to build a world with a single ball;
+		Build the world with on movable rectangle;
+		Build the world with one destructable rectangle;
+		Add keycode `space` to accelerate the ball from
+			our movable rectangle;
+		IFF ball hits bottom and no more balls, reset game;
+	*/
+	constructor(props={}){
+		super(props);
+	}
+	makeDestructibleRects(){
+		const middleCords		= getMiddleOfCanvas(this.width, this.height);
+		const yTop				= middleCords.y - Math.floor(this.height/4);
+		const hPadding			= 50;
+		const vPadding			= 2;
+		const hPossibleRects	= Math.floor(this.width / (this.brickWidth + hPadding));
+		const vPossibleRects	= Math.floor(this.height / (this.brickHeight + vPadding + 20))
+		for (let i=0; i< hPossibleRects; i++){
+			const bx = (this.brickWidth * i) + (hPadding * (i+1))
+			for (let j=0; j<vPossibleRects; j++){
+				const by = yTop + (this.brickHeight * j) + (vPadding * j+1)
+				const rectangle	= new Rectangle({
+					rectID:	this.rectangles.length,
+					color:	'white',
+					xLeft:	bx,
+					yTop:		by,
+					width:	this.brickWidth,
+					height:	this.brickHeight,
+				});
+				rectangle.isDestructible	= true;
+				rectangle.isDraggable		= false;
+				this.rectangles.push(rectangle);
+			}//end j-for
+		}//end i-for
+		const paddleBottomY = this.height + this.rectangles[0].height - 0.01;
+		this.rectangles[0].processDrag(
+			this.rectangles[0].xLeft, 
+			paddleBottomY, 
+			[]
+		);
+		this.rectangles[0].handleMove(
+			this.width, 
+			this.height,
+			[]
+		);
+
+		return this.rectangles.length-1;
+	}
+}//end Level5 Class
