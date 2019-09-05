403c9cd46b047439a1f4e14151b568dac5ef13ae
twoody <tanner.l.woody@gmail.com>
2019-09-05 11:38:02 -0700

    white space: tab to spaceX2;

--- code-wars/count-the-smiley-faces/index.php
+++ code-wars/count-the-smiley-faces/index.php
@@ -1,46 +1,46 @@
 <?php
 function count_smileys($arr): int {
-	$r					= 0;
-	$l					= count($arr);
-	$validEyes		= [":", ";"];
-	$validNoses		= ["-", "~"];
-	$validMouths	= [")", "D"];
-	for($i=0; $i<$l; $i++){
-		$t = str_split( $arr[$i] );
-		$onMouth			= FALSE;
-		$didNose			= FALSE;
-		$onEyes			= TRUE;
-		for ($j=0; $j<count($t); $j++ ){
-			$c = $t[$j];
-			if( $onEyes ){
-				if( in_array($c, $validEyes) === TRUE ){
-					$onEyes	= FALSE;
-					$didNose	= FALSE;
-					$onMouth	= TRUE;
-				}
-				else{
-					break;
-				}
-			}
-			elseif( $onMouth ){
-				if( $didNose === FALSE && in_array($c, $validNoses) ){
-					$didNose = TRUE;
-					continue;
-				}
-				else{
-					if( in_array($c, $validMouths) )
-						$r += 1;
-					$onMouth	= FALSE;
-					$onEyes	= TRUE;
-					$didNose	= FALSE;
-				}
-				break;
-			}
-			else
-				echo "ERROR: broke out\n";
-		}//end j-for
-	}//end i-for
-	return $r;
+  $r            = 0;
+  $l            = count($arr);
+  $validEyes    = [":", ";"];
+  $validNoses   = ["-", "~"];
+  $validMouths  = [")", "D"];
+  for($i=0; $i<$l; $i++){
+    $t = str_split( $arr[$i] );
+    $onMouth     = FALSE;
+    $didNose     = FALSE;
+    $onEyes      = TRUE;
+    for ($j=0; $j<count($t); $j++ ){
+      $c = $t[$j];
+      if( $onEyes ){
+        if( in_array($c, $validEyes) === TRUE ){
+          $onEyes  = FALSE;
+          $didNose = FALSE;
+          $onMouth = TRUE;
+        }
+        else{
+          break;
+        }
+      }
+      elseif( $onMouth ){
+        if( $didNose === FALSE && in_array($c, $validNoses) ){
+          $didNose = TRUE;
+          continue;
+        }
+        else{
+          if( in_array($c, $validMouths) )
+            $r += 1;
+          $onMouth = FALSE;
+          $onEyes  = TRUE;
+          $didNose = FALSE;
+        }
+        break;
+      }
+      else
+        echo "ERROR: broke out\n";
+    }//end j-for
+  }//end i-for
+  return $r;
 }
 $out1 = count_smileys( [] );
 $out2 = count_smileys( [':)'] );
