467ca156829eaeb68cdd6e746642f87b0131fb91
twoody <tanner.l.woody@gmail.com>
2019-10-03 20:38:49 -0700

    nice form styling

--- precoa/2019-row/data-entry/index.php
+++ precoa/2019-row/data-entry/index.php
@@ -1,3 +1,51 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+  <style>
+    .previous-results{
+      color: green;
+    }
+    .thickline{
+    border: 10px solid green;
+    border-radius: 5px;  
+    }
+    input[type=text], select {
+      width: 100%;
+      padding: 12px 20px;
+      margin: 8px 0;
+      display: inline-block;
+      border: 1px solid #ccc;
+      border-radius: 4px;
+      box-sizing: border-box;
+    }
+    
+    input[type=submit] {
+      width: 100%;
+      background-color: #4CAF50;
+      color: white;
+      padding: 14px 20px;
+      margin: 8px 0;
+      border: none;
+      border-radius: 4px;
+      cursor: pointer;
+    }
+    
+    input[type=submit]:hover {
+      background-color: #45a049;
+    }
+    
+    .data-form {
+      border-radius: 5px;
+      background-color: #f2f2f2;
+      padding: 20px;
+    }
+  </style>
+</head>
+<body>
+
+<div class="previous-results">
+  <!-- Put the previous querry success here or Error or nothing entered -->
 <?php
   /**
     Author:
@@ -13,13 +61,13 @@
   */
 function seconds_to_ms($score){
   $values  = explode(":", $score);
-  var_dump($values);
   $minutes = $values[0];
   $seconds = $values[1];
   $ms      = $values[2];
   $total   = ($minutes*60*1000) + ($seconds*1000) + ($ms*10);
   return $total;
 }
+$html = "";
 if( isset( $_GET['score'] ) ){
   //TODO: Check if result is already in the DB...
   //TODO: Validate that score is correct syntax and properly error report if not;
@@ -40,43 +88,41 @@ if( isset( $_GET['score'] ) ){
   $stmt->bindParam(3, $lname);
   $stmt->bindParam(4, $gender);
   $stmt->bindParam(5, $score_ms);
-
   $stmt->execute();
+  $conn->close();
+
+  $html .= $fname . " " . $lname . " ADDED TO THE DATABASE";
+  $html .= "<br/>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;SCORE - " . $score;
 }//end if GET
- 
+else{
+  $html .= "No entries currently added to the DB.";
+}
+
+echo $html;
 ?>
-<!DOCTYPE html>
-<html>
-<body>
-<h2>2019 Precoa Rowing Competition Data Entry</h2>
-<div class="previous-results">
-  <!-- Put the previous querry success here or Error or nothing entered -->
 </div>
-<form>
+<hr class="thickline">
+<h2>2019 Precoa Rowing Competition Data Entry</h2>
+<div class="data-form">
+  <form>
+    <label for="fname">First Name</label>
+    <input type="text" id="fname" name="fname" placeholder="Your name..">
 
-  <label for="score">Score</label>
-  <input type="text" name="score" id="score">
-  <br/>
-  <label for="fname">First Name</label>
-  <input type="text" name="fname" id="fname">
-  <br/>
-  <label for="fname">Last Name</label>
-  <input type="text" name="lname" id="lname">
-  <br/>
-  <br/>
+    <label for="lname">Last Name</label>
+    <input type="text" id="lname" name="lname" placeholder="Your last name..">
 
-  <span class="form-text">Gender:</span>
-  <br/>
+    <label for="score">Score</label>
+    <input type="text" id="score"  name="score" placeholder="MM:SS:MS">
 
-  <label for="male">Male</label>
-  <input type="radio" name="gender" id="male" value="m">
-  <br/>
-  <label for="female">Female</label>
-  <input type="radio" name="gender" id="female" value="f">
-  <br/>
+    <label for="country">Gender</label>
+    <select id="gender" name="gender">
+      <option value="f">Female</option>
+      <option value="m">Male</option>
+    </select>
 
-  <input type="submit" value="Submit">
-</form>
+    <input type="submit" value="Submit">
+  </form>
+</div>
 </body>
 </html>
 
