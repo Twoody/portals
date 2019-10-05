64676d4e628d26bfd6bb0bd41f0b19382eef8e7b
twoody <tanner.l.woody@gmail.com>
2019-10-05 14:57:03 -0700

    Pause Button coordinates moved to HUD properties;
    By moving the coordinates to the properties level and scope of HUD.js,
    we will be able to accurately detect if the button within these boundaries
    is being clicked or not;

--- react/tests/game-1/react-src/src/HUD.js
+++ react/tests/game-1/react-src/src/HUD.js
@@ -2,21 +2,23 @@ import { writeToScreen } from "./utils.js";
 
 export class HUD{
   constructor( props={} ){
-    this.x             = null;
-    this.y             = null;
-    this.w             = null;
-    this.sw            = null;
-    this.ballMsg       = null;
-    this.brickMsg      = null;
-    this.bottom        = null;
-    this.RALWidth      = 30; //(R)ectangle (a)nd (l)ives width;
-    this.pauseWidth    = 10;
-    this.h             = 13;
-    this.ballTemplate  = "BALLS: ";
-    this.brickTemplate = "BRICKS: ";
-    this.background    = "grey";
-    this.fontColor     = "orange";
-    this.font          = "13px Arial";
+    this.x                 = null;
+    this.y                 = null;
+    this.w                 = null;
+    this.sw                = null;
+    this.ballMsg           = null;
+    this.brickMsg          = null;
+    this.bottom            = null;
+    this.RALWidth          = 30; //(R)ectangle (a)nd (l)ives width;
+    this.pauseWidth        = 30;
+    this.hBuffer_pause     = 5;
+    this.middleWidthBuffer = 30;
+    this.h                 = 13;
+    this.ballTemplate      = "BALLS: ";
+    this.brickTemplate     = "BRICKS: ";
+    this.background        = "grey";
+    this.fontColor         = "orange";
+    this.font              = "13px Arial";
   }//end constructor
   updateDisplay(sWidth, ballCount, brickCount, score){
     this.ballMsg    = this.ballTemplate + ballCount;
@@ -42,12 +44,14 @@ export class HUD{
       }
       this.brickMsg += brickCount;
     }
-    this.w      = 10 * (maxLength - 1);
-    this.x      = sWidth - this.w + 16;
-    this.sw     = sWidth;
-    this.y      = 15;
-    this.bottom = this.y*2 + 4;
-    this.score  = score;
+    this.w           = 10 * (maxLength - 1);
+    this.x           = sWidth - this.w + 16;
+    this.sw          = sWidth;
+    this.y           = 15;
+    this.bottom      = this.y*2 + 4;
+    this.score       = score;
+    this.xLeft_pause = this.sw/2 - (this.RALWidth) - this.middleWidthBuffer -this.pauseWidth;
+    this.yTop_pause  = this.bottom/2+5;
   }//end updateDisplay();
   labelBallCnt(ctx){
     ctx.beginPath();
@@ -85,7 +89,6 @@ export class HUD{
     * Hearts and Missiles located in middle of screen
     */
     const hBuffer = 5;
-    const vBuffer = 10;
     const x       = this.sw/2 - this.RALWidth;
     const y       = 0;
     const rw      = this.RALWidth*2; //Rectangle width;
@@ -117,14 +120,10 @@ export class HUD{
     );
   }//end labelLivesAndRockets()
   labelPause(ctx){
-    const hBuffer = 5;
-    const vBuffer = 10;
-    const x       = this.sw/2 - this.rocketWidth - this.pauseWidth;
-    const y       = 0;
     ctx.beginPath();
     ctx.rect(
-      x,
-      y,
+      this.xLeft_pause,
+      0,
       this.pauseWidth,
       this.bottom //length
     );
@@ -134,11 +133,11 @@ export class HUD{
 
     writeToScreen(
       ctx, 
-      "8" + " " + "\uf102",
-      x + hBuffer, 
-      this.bottom - 4,                //vertical buffer
+      "\uf28b",
+      this.xLeft_pause + this.hBuffer_pause, 
+      this.yTop_pause,
       "orange", 
-      "900 15px \"Font Awesome 5 Free\"",
+      "900 20px \"Font Awesome 5 Free\"",
     );
   }//end labelPause()
   layBackdrop(ctx){
